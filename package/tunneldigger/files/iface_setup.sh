c304a0d8b99914b5dbdf56c3b6e94c57b77e2b35
Jernej Kos <k@jst.sm>
2012-06-06 12:12:25 +0200

    Interface up scripts are not needed as new OpenWrt versions have netifd which monitors for interface up events via netlink.

--- package/tunneldigger/files/iface_setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# Call the hotplug network interface setup script so our new L2TPv3 tunnel
-# interface gets configured with required addresses
-ACTION="add" INTERFACE="$1" /sbin/hotplug-call net
