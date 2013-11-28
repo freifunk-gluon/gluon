c03843eac17deae1d16ed385685b171aff80b423
Nils Schneider <nils@nilsschneider.net>
2013-11-28 21:49:34 +0100

    gluon-luci-autoupdater: simple luci module for autoupdater

---
+++ package/gluon-luci-autoupdater/files/usr/lib/lua/luci/model/cbi/admin/autoupdater.lua
@@ -0,0 +1,35 @@
+--[[
+LuCI - Lua Configuration Interface
+
+Copyright 2013 Nils Schneider <nils@nilsschneider.net>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+$Id$
+]]--
+
+m = Map("autoupdater", "Autoupdater")
+
+s = m:section(TypedSection, "autoupdater", "Einstelleungen")
+s.addremove = false
+
+s:option(Flag, "enabled", "Aktivieren")
+f = s:option(ListValue, "branch", "Branch")
+
+uci.cursor():foreach("autoupdater", "branch", function (section) f:value(section[".name"]) end)
+
+s = m:section(TypedSection, "branch", "Branches")
+s.addremove = true
+
+s:option(Value, "url", "URL")
+s:option(Value, "probability", "Update Wahrscheinlichkeit")
+s:option(Value, "good_signatures", "Benötigte Signaturen")
+
+o = s:option(DynamicList, "pubkey", "Public Keys")
+
+return m
+
