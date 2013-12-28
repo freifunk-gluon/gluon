a6ae2786f389348dc8c40816dfcf3d1e06bd4db8
Matthias Schiffer <mschiffer@universe-factory.net>
2013-12-28 17:17:02 +0100

    Make patch generation deterministic

--- scripts/update-patches.sh
+++ scripts/update-patches.sh
@@ -7,9 +7,12 @@ shopt -s nullglob
 
 for module in $GLUON_MODULES; do
 	dir="$1"/$module
-	git -C "$dir" checkout patched
-
 	rm -f "$1"/patches/$module/*.patch
 	mkdir -p "$1"/patches/$module
-	git -C "$dir" format-patch -o "$1"/patches/$module base 
+
+	n=0
+	for commit in $(git -C "$dir" rev-list --reverse --no-merges base..patched); do
+		let n=n+1
+		git -C "$dir" show --pretty=format:'From: %an <%ae>%nDate: %aD%nSubject: %B' $commit > "$1"/patches/$module/"$(printf '%04u' $n)-$(git -C "$dir" show -s --pretty=format:%f).patch"
+	done
 done
