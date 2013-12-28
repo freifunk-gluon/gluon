76167579cd20049547b5c1fb890208f598525dc9
Matthias Schiffer <mschiffer@universe-factory.net>
2013-12-28 22:43:16 +0100

    Fix branch name for modules without patches

--- scripts/patch.sh
+++ scripts/patch.sh
@@ -9,13 +9,14 @@ for module in $GLUON_MODULES; do
 	dir="$1"/$module
 	git -C $dir checkout -B patching base
 
-	if [ -z "$1"/patches/$module/*.patch ]; then continue; fi
-	git -C "$dir" am "$1"/patches/$module/*.patch || (
-		git -C "$dir" am --abort
-		git -C "$dir" checkout patched
-		git -C "$dir" branch -D patching
-		false
-	)
+	if [ "$1"/patches/$module/*.patch ]; then
+		git -C "$dir" am "$1"/patches/$module/*.patch || (
+			git -C "$dir" am --abort
+			git -C "$dir" checkout patched
+			git -C "$dir" branch -D patching
+			false
+		)
+	fi
 	git -C "$dir" checkout -B patched
 	git -C "$dir" branch -d patching
 done
