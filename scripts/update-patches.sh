b7676e5818d22c63f20c52bc0f3fb7910ed98b37
Matthias Schiffer <mschiffer@universe-factory.net>
2013-12-28 23:07:01 +0100

    Fix update-patches patch names

--- scripts/update-patches.sh
+++ scripts/update-patches.sh
@@ -13,6 +13,6 @@ for module in $GLUON_MODULES; do
 	n=0
 	for commit in $(git -C "$dir" rev-list --reverse --no-merges base..patched); do
 		let n=n+1
-		git -C "$dir" show --pretty=format:'From: %an <%ae>%nDate: %aD%nSubject: %B' $commit > "$1"/patches/$module/"$(printf '%04u' $n)-$(git -C "$dir" show -s --pretty=format:%f).patch"
+		git -C "$dir" show --pretty=format:'From: %an <%ae>%nDate: %aD%nSubject: %B' $commit > "$1"/patches/$module/"$(printf '%04u' $n)-$(git -C "$dir" show -s --pretty=format:%f $commit).patch"
 	done
 done
