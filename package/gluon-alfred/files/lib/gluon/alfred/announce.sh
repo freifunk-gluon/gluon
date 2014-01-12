d1f01a588fb6089b29b4243140e2d0c94e995fae
Matthias Schiffer <mschiffer@universe-factory.net>
2014-01-12 22:35:00 +0100

    Clean up gluon-alfred
    * Merge gluon-alfred-ffmap into gluon-alfred
    * Use gluon-crond instead of alfred's "facter" mechanism based on the broken busybox crond

--- package/gluon-alfred/files/lib/gluon/alfred/announce.sh
+++ package/gluon-alfred/files/lib/gluon/alfred/announce.sh
@@ -16,6 +16,7 @@ set -e
 json_init
 json_add_string "name" "$(uci get 'system.@system[0].hostname')"
 if [ "$(uci get 'system.@system[0].share_location')" = 1 ]; then
-	json_add_string "gps" "$(uci get 'system.@system[0].latitude') $(uci get 'system.@system[0].longitude')"
-fi 
+	json_add_string "location" "$(uci get 'system.@system[0].latitude') $(uci get 'system.@system[0].longitude')"
+fi
+
 json_dump | tr -d '\n' | alfred -s "$ALFRED_DATA_TYPE"
