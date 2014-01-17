ca95374ea1b0dcbe26991501e3a9dd07776eba76
Jan-Philipp Litza <janphilipp@litza.de>
2014-01-17 17:23:58 +0100

    Enable the site config to include own package feeds

--- scripts/modules.sh
+++ scripts/modules.sh
@@ -1,7 +1,8 @@
 . "$1"/modules
+[ ! -f "$1"/site/modules ] || . "$1"/site/modules
 
 GLUON_MODULES=openwrt
 
-for feed in $GLUON_FEEDS; do
+for feed in $GLUON_SITE_FEEDS $GLUON_FEEDS; do
 	GLUON_MODULES="$GLUON_MODULES packages/$feed"
 done
