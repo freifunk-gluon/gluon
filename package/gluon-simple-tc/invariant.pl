8f7b59d787635ebeb23f34ade54c3ea80a9efd41
Matthias Schiffer <mschiffer@universe-factory.net>
2014-01-19 23:33:52 +0100

    gluon-simple-tc: add missing defaults on every upgrade

--- package/gluon-simple-tc/invariant.pl
+++ package/gluon-simple-tc/invariant.pl
@@ -1,23 +1,18 @@
 my $cfg = $CONFIG->{simple_tc};
 
-print <<'END';
-#/bin/sh
-
-uci -q batch <<EOF
-END
+print "#/bin/sh\n\n";
 
 foreach my $name (sort keys %{$cfg}) {
   my $interface = $cfg->{$name};
 
+  print "uci -q get gluon-simple-tc.$name >/dev/null || uci -q batch <<EOF\n";
   print "set gluon-simple-tc.$name=interface\n";
 
   for (qw(enabled ifname limit_egress limit_ingress)) {
     print "set gluon-simple-tc.$name.$_=$interface->{$_}\n";
   }
+
+  print "EOF\n\n";
 }
 
-print <<END;
-
-commit gluon-simple-tc
-EOF
-END
+print "uci commit gluon-simple-tc\n";
