ce1a160bcc4445bcd67ab78dbb5b6ffbf8d96e9a
Nils Schneider <nils@nilsschneider.net>
2014-07-27 20:05:12 +0200

    gluon-config-mode: mention reboot on button
    Make it clear that saving settings will reboot the device.

--- package/gluon-config-mode/files/usr/lib/lua/luci/model/cbi/gluon-config-mode/wizard.lua
+++ package/gluon-config-mode/files/usr/lib/lua/luci/model/cbi/gluon-config-mode/wizard.lua
@@ -5,7 +5,7 @@ local f, s, o
 f = SimpleForm("wizard")
 f.reset = false
 f.template = "gluon-config-mode/cbi/wizard"
-f.submit = "Fertig"
+f.submit = "Speichern &amp; Neustarten"
 
 s = f:section(SimpleSection, nil, nil)
 
