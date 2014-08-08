c035ad0574025497ef96a2d98768a0f16de67aad
Matthias Schiffer <mschiffer@universe-factory.net>
2014-08-08 20:17:30 +0200

    Remove BROKEN for mpc85xx-generic target

--- targets/targets.mk
+++ targets/targets.mk
@@ -1,6 +1,6 @@
 $(eval $(call GluonTarget,ar71xx,generic))
+$(eval $(call GluonTarget,mpc85xx,generic))
 
 ifneq ($(BROKEN),)
 $(eval $(call GluonTarget,x86,generic))
-$(eval $(call GluonTarget,mpc85xx,generic))
 endif
