024d3e3c99f7ae17b19f69404cf9b0b644871de1
Matthias Schiffer <mschiffer@universe-factory.net>
2014-08-08 17:57:18 +0200

    Add experimental support for the TL-WDR4900 (MPC85xx target)

---
+++ targets/mpc85xx-generic/profiles.mk
@@ -0,0 +1,7 @@
+# List of hardware profiles
+
+## TP-Link
+
+# TL-WDR4900 v1
+$(eval $(call GluonProfile,TLWDR4900))
+$(eval $(call GluonModel,TLWDR4900,tl-wdr4900-v1-squashfs,tp-link-tl-wdr4900-v1))
--- targets/targets.mk
+++ targets/targets.mk
@@ -2,4 +2,5 @@ $(eval $(call GluonTarget,ar71xx,generic))
 
 ifneq ($(BROKEN),)
 $(eval $(call GluonTarget,x86,generic))
+$(eval $(call GluonTarget,mpc85xx,generic))
 endif
