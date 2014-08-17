54f577e9313d6cc7bfb5f749a14a42e85ccc1fe2
Matthias Schiffer <mschiffer@universe-factory.net>
2014-08-17 19:55:45 +0200

    Add experimental support for the Ubiquiti UniFi AP Pro

--- targets/ar71xx-generic/profiles.mk
+++ targets/ar71xx-generic/profiles.mk
@@ -85,6 +85,9 @@ $(eval $(call GluonModel,UBNT,ubnt-bullet-m-squashfs,ubiquiti-bullet-m))
 $(eval $(call GluonModel,UBNT,ubnt-nano-m-squashfs,ubiquiti-nanostation-m))
 $(eval $(call GluonModel,UBNT,ubnt-unifi-squashfs,ubiquiti-unifi))
 $(eval $(call GluonModel,UBNT,ubnt-unifi-outdoor-squashfs,ubiquiti-unifiap-outdoor))
+ifeq ($(BROKEN),1)
+$(eval $(call GluonModel,UBNT,ubnt-uap-pro-squashfs,ubiquiti-unifi-ap-pro))
+endif
 
 
 ## D-Link
