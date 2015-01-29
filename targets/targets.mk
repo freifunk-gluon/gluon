3056e8bcee8e326df1f019aa688a5fecfef89cb7
Matthias Schiffer <mschiffer@universe-factory.net>
2015-01-29 07:49:00 +0100

    x86-generic: remove BROKEN check

--- targets/targets.mk
+++ targets/targets.mk
@@ -1,7 +1,7 @@
 $(eval $(call GluonTarget,ar71xx,generic))
 $(eval $(call GluonTarget,mpc85xx,generic))
+$(eval $(call GluonTarget,x86,generic))
 
 ifneq ($(BROKEN),)
 $(eval $(call GluonTarget,ramips,rt305x))
-$(eval $(call GluonTarget,x86,generic))
 endif
