35ef35351e3657e9734e39574e84fd6965aa78d7
Matthias Schiffer <mschiffer@universe-factory.net>
2015-02-06 20:15:55 +0100

    Add support for TP-LINK TL-WA901ND v3

--- targets/ar71xx-generic/profiles.mk
+++ targets/ar71xx-generic/profiles.mk
@@ -88,6 +88,7 @@ $(eval $(call GluonModel,TLWA860,tl-wa860re-v1-squashfs,tp-link-tl-wa860re-v1))
 # TL-WA901N/ND v2
 $(eval $(call GluonProfile,TLWA901))
 $(eval $(call GluonModel,TLWA901,tl-wa901nd-v2-squashfs,tp-link-tl-wa901n-nd-v2))
+$(eval $(call GluonModel,TLWA901,tl-wa901nd-v3-squashfs,tp-link-tl-wa901n-nd-v3))
 
 # TL-MR3020 v1
 $(eval $(call GluonProfile,TLMR3020))
