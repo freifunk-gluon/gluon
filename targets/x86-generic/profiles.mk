20a8c95eb58618feb5f976f9eae043a5b5f484af
Matthias Schiffer <mschiffer@universe-factory.net>
2015-03-15 18:26:29 +0100

    x86-{generic,kvm_guest}: generate sysupgrade images
    No autoupdater support yet, but that will follow.

--- targets/x86-generic/profiles.mk
+++ targets/x86-generic/profiles.mk
@@ -1,14 +1,14 @@
 $(eval $(call GluonProfile,GENERIC,kmod-3c59x kmod-e100 kmod-e1000 kmod-natsemi kmod-ne2k-pci kmod-pcnet32 kmod-8139too kmod-r8169 kmod-sis900 kmod-tg3 kmod-via-rhine kmod-via-velocity))
 $(eval $(call GluonProfileFactorySuffix,GENERIC,-ext4,.img.gz))
-$(eval $(call GluonProfileSysupgradeSuffix,GENERIC))
+$(eval $(call GluonProfileSysupgradeSuffix,GENERIC,-ext4,.img.gz))
 $(eval $(call GluonModel,GENERIC,combined,x86-generic))
 
 $(eval $(call GluonProfile,VDI,kmod-pcnet32 kmod-e1000))
 $(eval $(call GluonProfileFactorySuffix,VDI,-ext4,.vdi))
-$(eval $(call GluonProfileSysupgradeSuffix,VDI))
+$(eval $(call GluonProfileSysupgradeSuffix,VDI,-ext4,.img.gz))
 $(eval $(call GluonModel,VDI,combined,x86-virtualbox))
 
 $(eval $(call GluonProfile,VMDK,kmod-pcnet32))
 $(eval $(call GluonProfileFactorySuffix,VMDK,-ext4,.vmdk))
-$(eval $(call GluonProfileSysupgradeSuffix,VMDK))
+$(eval $(call GluonProfileSysupgradeSuffix,VMDK,-ext4,.img.gz))
 $(eval $(call GluonModel,VMDK,combined,x86-vmware))
