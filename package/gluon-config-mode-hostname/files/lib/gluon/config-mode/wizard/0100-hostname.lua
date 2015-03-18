325eeb4f69c7941b4f4ad623e1976e04cde92367
Matthias Schiffer <mschiffer@universe-factory.net>
2015-03-19 00:57:16 +0100

    gluon-config-mode-hostname: add translations

--- package/gluon-config-mode-hostname/files/lib/gluon/config-mode/wizard/0100-hostname.lua
+++ package/gluon-config-mode-hostname/files/lib/gluon/config-mode/wizard/0100-hostname.lua
@@ -1,11 +1,12 @@
 local cbi = require "luci.cbi"
+local i18n = require "luci.i18n"
 local uci = luci.model.uci.cursor()
 
 local M = {}
 
 function M.section(form)
   local s = form:section(cbi.SimpleSection, nil, nil)
-  local o = s:option(cbi.Value, "_hostname", "Name dieses Knotens")
+  local o = s:option(cbi.Value, "_hostname", i18n.translate("Node name"))
   o.value = uci:get_first("system", "system", "hostname")
   o.rmempty = false
   o.datatype = "hostname"
