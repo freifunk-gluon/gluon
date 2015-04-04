6a7c24506fcd43f7ca83fa5c41eaeff3d006fcca
Nils Schneider <nils@nilsschneider.net>
2015-04-04 22:36:36 +0200

    ar71xx: Meraki MR12/MR62/MR16/MR66 support
    OpenWRT now supports the CISCO Meraki enterprise class routers
    MR12, MR16, MR62 and MR66. The fabric firmware demands the yearly
    renewal of a support license.
    
    This firmware was successfully tested by @Garunda for the MR62 (and
    the MR12 with it for which this is an alias). The initial firmware
    pre OpenWRT adoption was prepared and adapted for Gluon by @tcatm.
    The confirmation of the functionality of the image for the MR66
    (and the aliased MR16 with it) is still pending.
    
    The devices are of strategic interest to the Freifunk community as
    they are making a rock-solid impression. However, these come with
    fairly hefty annual license. The Freifunk may offer an escape route
    for those who had signed up and want to keep their investment into the
    similarly expensive hardware. Used evices sell for $60 on eBay/Amazon
    in the US. Here in the old world it is all >300 €, still.
    
    Credits go to @Garunda for testing, to @tcatm for finding the
    OpenWRT patch prior to its adoption and preparing the initial Gluon
    adaptation, to @smoe for the update once that patch had arrived in
    OpenWRT, and to @NeoRaider for his review and advice to use
    GluonModelAlias for MR62 and MR66 to point to MR12 and MR16,
    respectively.

--- targets/ar71xx-generic/profiles.mk
+++ targets/ar71xx-generic/profiles.mk
@@ -288,3 +288,16 @@ $(eval $(call GluonModel,HORNETUB,hornet-ub,alfa-hornet-ub))
 $(eval $(call GluonModelAlias,HORNETUB,alfa-hornet-ub,alfa-ap121))
 $(eval $(call GluonModelAlias,HORNETUB,alfa-hornet-ub,alfa-ap121u))
 
+## Meraki
+
+# Meraki MR12/MR62
+$(eval $(call GluonProfile,MR12,rssileds))
+$(eval $(call GluonProfileFactorySuffix,MR12))
+$(eval $(call GluonModel,MR12,mr12,meraki-mr12))
+$(eval $(call GluonModelAlias,MR12,meraki-mr12,meraki-mr62))
+
+# Meraki MR16/MR66
+$(eval $(call GluonProfile,MR16,rssileds))
+$(eval $(call GluonProfileFactorySuffix,MR16))
+$(eval $(call GluonModel,MR16,mr16,meraki-mr16))
+$(eval $(call GluonModelAlias,MR16,meraki-mr16,meraki-mr66))
