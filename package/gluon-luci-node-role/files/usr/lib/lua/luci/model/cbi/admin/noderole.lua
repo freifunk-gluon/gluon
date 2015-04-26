0c80e7863f1745bebcfb26192c5996a275526fa4
Kokel <tobias@hachmer.de>
2015-04-26 17:34:05 +0200

    gluon-luci-node-role: add translations

--- package/gluon-luci-node-role/files/usr/lib/lua/luci/model/cbi/admin/noderole.lua
+++ package/gluon-luci-node-role/files/usr/lib/lua/luci/model/cbi/admin/noderole.lua
@@ -1,23 +1,22 @@
 local f, s, o
 local site = require 'gluon.site_config'
+local i18n = require "luci.i18n"
 local uci = luci.model.uci.cursor()
 local config = 'gluon-node-info'
 
 -- where to read the configuration from
 local role = uci:get(config, uci:get_first(config, "system"), "role")
 
-f = SimpleForm("role", "Verwendungszweck")
+f = SimpleForm("role", i18n.translate("Node role"))
 f.reset = false
 f.template = "admin/expertmode"
-f.submit = "Fertig"
 
-s = f:section(SimpleSection, nil, [[
-Wenn dein Freifunk-Router eine besondere Rolle im Freifunk Netz einnimmt, kannst du diese hier angeben.
-Bringe bitte zuvor in Erfahrung welche Auswirkungen die zur Verfügung stehenden Rollen im Freifunk-Netz haben.
-Setze die Rolle nur, wenn du weißt was du machst.
-]])
+s = f:section(SimpleSection, nil, i18n.translate(
+  "If this node has a special role within the freifunk network you can specify this role here. "
+    .. "Please find out about the available roles and their impact first. "
+    .. "Only change the role if you know what you are doing."))
 
-o = s:option(ListValue, "role", "Rolle")
+o = s:option(ListValue, "role", i18n.translate("Role"))
 o.default = role
 o.rmempty = false
 for role, prettyname in pairs(site.roles.list) do
