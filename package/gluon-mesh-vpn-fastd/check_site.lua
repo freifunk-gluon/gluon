a50cce4e0975afb9c93ffa40eb361825d407c394
Matthias Schiffer <mschiffer@universe-factory.net>
2015-05-02 22:55:23 +0200

    gluon-mesh-vpn-fastd: make adding or removing the null method survive updates (if desired)

--- package/gluon-mesh-vpn-fastd/check_site.lua
+++ package/gluon-mesh-vpn-fastd/check_site.lua
@@ -1,6 +1,7 @@
 need_string_array('fastd_mesh_vpn.methods')
 need_number('fastd_mesh_vpn.mtu')
 need_boolean('fastd_mesh_vpn.enabled', false)
+need_boolean('fastd_mesh_vpn.configurable', false)
 
 
 local function check_peer(prefix)
