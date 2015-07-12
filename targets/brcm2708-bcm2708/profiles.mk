7d7c093007fabfdad890fe5976e476ed25bce57f
Matthias Schiffer <mschiffer@universe-factory.net>
2015-07-12 18:46:30 +0200

    brcm2708-bcm2708/-bcm2709: remove unnecessary package list
    These packages are included by OpenWrt by default on this target and don't
    need to be specified manually.

--- targets/brcm2708-bcm2708/profiles.mk
+++ targets/brcm2708-bcm2708/profiles.mk
@@ -1,4 +1,4 @@
-$(eval $(call GluonProfile,RaspberryPi,brcm2708-gpu-fw kmod-usb-hid kmod-sound-core kmod-sound-arm-bcm2835))
+$(eval $(call GluonProfile,RaspberryPi))
 $(eval $(call GluonProfileFactorySuffix,RaspberryPi,-vfat-ext4,.img.gz))
 $(eval $(call GluonProfileSysupgradeSuffix,RaspberryPi))
 $(eval $(call GluonModel,RaspberryPi,sdcard,raspberry-pi))
--- targets/brcm2708-bcm2709/profiles.mk
+++ targets/brcm2708-bcm2709/profiles.mk
@@ -1,4 +1,4 @@
-$(eval $(call GluonProfile,RaspberryPi2,brcm2708-gpu-fw kmod-usb-hid kmod-sound-core kmod-sound-arm-bcm2835))
+$(eval $(call GluonProfile,RaspberryPi2))
 $(eval $(call GluonProfileFactorySuffix,RaspberryPi2,-vfat-ext4,.img.gz))
 $(eval $(call GluonProfileSysupgradeSuffix,RaspberryPi2))
 $(eval $(call GluonModel,RaspberryPi2,sdcard,raspberry-pi-2))
