6f1fa9d8a10a9e07ca4830990073b6ae8d96df04
Nils Schneider <nils@nilsschneider.net>
2015-08-16 01:04:35 +0200

    gluon-mesh-babel: some initial work

--- package/gluon-next-node-batman-adv/check_site.lua
+++
@@ -1,4 +0,0 @@
-need_string_match('next_node.ip4', '^%d+.%d+.%d+.%d+$')
-need_string_match('next_node.ip6', '^[%x:]+$')
-
-need_string_match('next_node.mac', '^%x[02468aAcCeE]:%x%x:%x%x:%x%x:%x%x:%x%x$')
