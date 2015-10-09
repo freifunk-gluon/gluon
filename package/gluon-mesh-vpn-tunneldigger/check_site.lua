df8aaf290c04ef7d2a058aa9c475d02d322bea4b
CyrusFox <cyrus@lambdacore.de>
2015-10-09 10:51:39 +0200

    Fixed check_site.lua

--- package/gluon-mesh-vpn-tunneldigger/check_site.lua
+++ package/gluon-mesh-vpn-tunneldigger/check_site.lua
@@ -1,3 +1,3 @@
 need_number('tunneldigger_mesh_vpn.mtu')
-need_boolean('tunneldigger_mesh_vpn.enabled', false)
-need_string_array('tunneldigger_mesh_vpn.brokers'))
+need_number('tunneldigger_mesh_vpn.enabled', false)
+need_string_array('tunneldigger_mesh_vpn.brokers')
