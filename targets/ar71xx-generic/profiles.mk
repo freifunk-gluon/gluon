7028c0e113ad5a4acc5d7a57ebb2f8dd1e8559f8
Matthias Schiffer <mschiffer@universe-factory.net>
2015-11-06 13:17:06 +0100

    ar71xx-generic: add profile for TL-WR1043ND v3

--- targets/ar71xx-generic/profiles.mk
+++ targets/ar71xx-generic/profiles.mk
@@ -78,10 +78,11 @@ $(eval $(call GluonModelAlias,TLWR941,tp-link-tl-wr941n-nd-v4,tp-link-tl-wr940n-
 $(eval $(call GluonModelAlias,TLWR941,tp-link-tl-wr941n-nd-v5,tp-link-tl-wr940n-nd-v2))
 $(eval $(call GluonModelAlias,TLWR941,tp-link-tl-wr941n-nd-v6,tp-link-tl-wr940n-nd-v3))
 
-# TL-WR1043N/ND v1, v2
+# TL-WR1043N/ND v1, v2, v3
 $(eval $(call GluonProfile,TLWR1043))
 $(eval $(call GluonModel,TLWR1043,tl-wr1043nd-v1,tp-link-tl-wr1043n-nd-v1))
 $(eval $(call GluonModel,TLWR1043,tl-wr1043nd-v2,tp-link-tl-wr1043n-nd-v2))
+$(eval $(call GluonModel,TLWR1043,tl-wr1043nd-v3,tp-link-tl-wr1043n-nd-v3))
 
 # TL-WDR3500/3600/4300 v1
 $(eval $(call GluonProfile,TLWDR4300))
