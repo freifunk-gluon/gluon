426f07755ddf60e7fc854333b870e943a88a4f9e
Matthias Schiffer <mschiffer@universe-factory.net>
2016-02-28 03:30:14 +0100

    scripts/patch.sh: add --committer-date-is-author-date to `git am`
    This option will make the generated commit IDs deterministic, greatly
    reducing the number of repository objects created when calling `make
    update` repeatedly.

--- scripts/patch.sh
+++ scripts/patch.sh
@@ -10,7 +10,7 @@ for module in $GLUON_MODULES; do
 	git checkout -B patching base
 
 	for patch in "$GLUONDIR"/patches/$module/*.patch; do
-		if ! git -c user.name='Gluon Patch Manager' -c user.email='gluon@void.example.com' am --whitespace=nowarn "$patch"; then
+		if ! git -c user.name='Gluon Patch Manager' -c user.email='gluon@void.example.com' am --whitespace=nowarn --committer-date-is-author-date "$patch"; then
 			git am --abort
 			git checkout patched
 			git branch -D patching
