b4a378d9d841e207dde850f4d48edd6070f629ce
Matthias Ludwig <matthias.ludwig@elektrobit.com>
2016-02-29 11:23:01 +0100

    ar71xx-generic: add support for TP-Link MR13U

--- targets/ar71xx-generic/profiles.mk
+++ targets/ar71xx-generic/profiles.mk
@@ -130,6 +130,10 @@ endif
 $(eval $(call GluonProfile,TLMR13U))
 $(eval $(call GluonModel,TLMR13U,tl-mr13u-v1,tp-link-tl-mr13u-v1))
 
+# TL-MR13U v1
+$(eval $(call GluonProfile,TLMR13U))
+$(eval $(call GluonModel,TLMR13U,tl-mr13u-v1,tp-link-tl-mr13u-v1))
+
 # TL-MR3020 v1
 $(eval $(call GluonProfile,TLMR3020))
 $(eval $(call GluonModel,TLMR3020,tl-mr3020-v1,tp-link-tl-mr3020-v1))
