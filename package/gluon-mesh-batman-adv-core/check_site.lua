691531de7e982ba0ac8485a044aeb7b2a8099b95
kb-light <freifunk@kb-light.de>
2016-08-03 11:03:44 +0200

    gluon-mesh-batman-adv-core: use need_one_of() to check for valid mcast_rate

--- package/gluon-mesh-batman-adv-core/check_site.lua
+++ package/gluon-mesh-batman-adv-core/check_site.lua
@@ -1,15 +1,18 @@
 for _, config in ipairs({'wifi24', 'wifi5'}) do
+   local rates = {1000, 2000, 5500, 6000, 9000, 11000, 12000, 18000, 24000, 36000, 48000, 54000}
+   rates = need_array_of(config .. '.supported_rates', rates, false) or rates
+
    if need_table(config .. '.ibss', nil, false) then
       need_string(config .. '.ibss.ssid')
       need_string_match(config .. '.ibss.bssid', '^%x[02468aAcCeE]:%x%x:%x%x:%x%x:%x%x:%x%x$')
-      need_number(config .. '.ibss.mcast_rate', false)
+      need_one_of(config .. '.ibss.mcast_rate', rates, false)
       need_number(config .. '.ibss.vlan', false)
       need_boolean(config .. '.ibss.disabled', false)
    end
 
    if need_table(config .. '.mesh', nil, false) then
       need_string(config .. '.mesh.id')
-      need_number(config .. '.mesh.mcast_rate', false)
+      need_one_of(config .. '.mesh.mcast_rate', rates, false)
       need_boolean(config .. '.mesh.disabled', false)
    end
 end
