d4bfa6c4e037234597d6d74d68eb8ed3782367c7
rubo77 <github@r.z11.de>
2016-08-30 13:40:06 +0200

    gluon-config-mode-hostname: clarify the use of the nodename

--- package/gluon-config-mode-hostname/luasrc/lib/gluon/config-mode/wizard/0100-hostname.lua
+++ package/gluon-config-mode-hostname/luasrc/lib/gluon/config-mode/wizard/0100-hostname.lua
@@ -3,6 +3,11 @@ return function(form, uci)
 
 	local pretty_hostname = require "pretty_hostname"
 
+	form:section(Section, nil, pkg_i18n.translate(
+		"The node name is used solely for identification of your node, e.g. on a "
+		.. "nodemap. It has no influence on the name (SSID) of the broadcasted WLAN."
+	))
+	
 	local s = form:section(Section)
 	local o = s:option(Value, "hostname", pkg_i18n.translate("Node name"))
 	o.default = pretty_hostname.get(uci)
