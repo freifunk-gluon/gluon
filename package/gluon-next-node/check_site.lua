b902ddd8df17b19404f4c127f55d035b36127c8b
Matthias Schiffer <mschiffer@universe-factory.net>
2016-09-10 17:15:16 +0200

    gluon-next-node: make IPv4 next-node address optional
    If next_node.ip4 is not set or gluon-next-node is not used, prefix4 will
    now be optional as well.

--- package/gluon-next-node/check_site.lua
+++ package/gluon-next-node/check_site.lua
@@ -1,4 +1,7 @@
-need_string_match('next_node.ip4', '^%d+.%d+.%d+.%d+$')
+if need_string_match('next_node.ip4', '^%d+.%d+.%d+.%d+$', false) then
+	need_string_match('prefix4', '^%d+.%d+.%d+.%d+/%d+$')
+end
+
 need_string_match('next_node.ip6', '^[%x:]+$')
 
 need_string_match('next_node.mac', '^%x[02468aAcCeE]:%x%x:%x%x:%x%x:%x%x:%x%x$')
