fb85d96d5e4007751ec5290f64cc5f5066e0e17a
Matthias Schiffer <mschiffer@universe-factory.net>
2016-11-30 15:27:33 +0100

    gluon-next-node: don't require next_node.ip6
    The next-node interface may be useful with IPv4 only or even just a MAC
    address. This prepares merging gluon-next-node into gluon-batman-adv-core.

--- package/gluon-next-node/check_site.lua
+++ package/gluon-next-node/check_site.lua
@@ -2,6 +2,6 @@ if need_string_match('next_node.ip4', '^%d+.%d+.%d+.%d+$', false) then
 	need_string_match('prefix4', '^%d+.%d+.%d+.%d+/%d+$')
 end
 
-need_string_match('next_node.ip6', '^[%x:]+$')
+need_string_match('next_node.ip6', '^[%x:]+$', false)
 
 need_string_match('next_node.mac', '^%x[02468aAcCeE]:%x%x:%x%x:%x%x:%x%x:%x%x$')
