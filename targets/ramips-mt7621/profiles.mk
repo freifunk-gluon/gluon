890ce568a44af81b400809c4fe10c26968d5aef9
Jan-Philipp Litza <janphilipp@litza.de>
2016-12-04 15:58:48 +0100

    Add support for ZBT-WG3526

--- targets/ramips-mt7621/profiles.mk
+++ targets/ramips-mt7621/profiles.mk
@@ -2,3 +2,9 @@
 
 $(eval $(call GluonProfile,Default))
 $(eval $(call GluonModel,Default,dir-860l-b1,d-link-dir-860l-b1))
+
+ifneq ($(BROKEN),)
+$(eval $(call GluonProfile,ZBT-WG3526)) # BROKEN: hangs during reboot (http://lists.infradead.org/pipermail/linux-mtd/2016-November/070368.html)
+$(eval $(call GluonProfileFactorySuffix,ZBT-WG3526))
+$(eval $(call GluonModel,ZBT-WG3526,zbt-wg3526,zbt-wg3526))
+endif
