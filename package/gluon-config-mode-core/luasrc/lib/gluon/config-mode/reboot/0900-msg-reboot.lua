ff0ad2075246913f869d7410d5b9b0cd37f713f4
Ruben Barkow <rubo77@users.noreply.github.com>
2016-12-11 03:13:28 +0100

    gluon-config-mode-core: hand over more info to the reboot screen (#930)

--- package/gluon-config-mode-core/luasrc/lib/gluon/config-mode/reboot/0900-msg-reboot.lua
+++ package/gluon-config-mode-core/luasrc/lib/gluon/config-mode/reboot/0900-msg-reboot.lua
@@ -1,3 +1,23 @@
 local i18n = require 'luci.i18n'
+local site = require 'gluon.site_config'
+local gluon_luci = require 'gluon.luci'
+local sysconfig = require 'gluon.sysconfig'
+local pretty_hostname = require 'pretty_hostname'
 
-return function () luci.template.render_string(i18n.translate('gluon-config-mode:reboot')) end
+local uci = luci.model.uci.cursor()
+
+local hostname = pretty_hostname.get(uci)
+local contact = uci:get_first('gluon-node-info', 'owner', 'contact')
+
+local msg = i18n.translate('gluon-config-mode:reboot')
+
+return function ()
+  luci.template.render_string(msg, {
+    hostname = hostname,
+    site = site,
+    sysconfig = sysconfig,
+    contact = contact,
+    escape = gluon_luci.escape,
+    urlescape = gluon_luci.urlescape,
+  })
+end
