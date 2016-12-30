a4f06228c9123608074685ac81f6ea4667fabd42
Christof Schulze <christof.schulze@gmx.net>
2016-12-30 17:09:35 +0100

    gluon-luci-portconfig: add vxlan config checkbox

--- package/gluon-luci-portconfig/luasrc/usr/lib/lua/luci/model/cbi/admin/portconfig.lua
+++ package/gluon-luci-portconfig/luasrc/usr/lib/lua/luci/model/cbi/admin/portconfig.lua
@@ -84,6 +84,10 @@ end
 
 s = f:section(SimpleSection, nil, nil)
 
+o = s:option(Flag, "mesh_vxlan", translate("Encapsulate mesh into vxlan"))
+o.default = uci:get_bool("network", uci:get_first("network","vxlan") , "active") and o.enabled or o.disabled
+o.rmempty = false
+
 o = s:option(Flag, "mesh_wan", translate("Enable meshing on the WAN interface"))
 o.default = uci:get_bool("network", "mesh_wan", "auto") and o.enabled or o.disabled
 o.rmempty = false
