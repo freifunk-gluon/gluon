e5cb125737b295f45c5ec86953ec17bf3504305b
Matthias Schiffer <mschiffer@universe-factory.net>
2017-02-10 21:19:34 +0100

    Move wifi24/5 and mesh_on_wan/lan site validators from gluon-mesh-batman-adv-core to gluon-core
    These values have been in use in gluon-core for some time, the validators
    need to be moved as well.

--- package/gluon-core/check_site.lua
+++ package/gluon-core/check_site.lua
@@ -48,3 +48,25 @@ if need_table('next_node', nil, false) then
 	need_string_match('next_node.ip6', '^[%x:]+$', false)
 	need_string_match('next_node.ip4', '^%d+.%d+.%d+.%d+$', false)
 end
+
+for _, config in ipairs({'wifi24', 'wifi5'}) do
+  local rates = {1000, 2000, 5500, 6000, 9000, 11000, 12000, 18000, 24000, 36000, 48000, 54000}
+  rates = need_array_of(config .. '.supported_rates', rates, false) or rates
+
+  if need_table(config .. '.ibss', nil, false) then
+    need_string(config .. '.ibss.ssid')
+    need_string_match(config .. '.ibss.bssid', '^%x[02468aAcCeE]:%x%x:%x%x:%x%x:%x%x:%x%x$')
+    need_one_of(config .. '.ibss.mcast_rate', rates, false)
+    need_number(config .. '.ibss.vlan', false)
+    need_boolean(config .. '.ibss.disabled', false)
+  end
+
+  if need_table(config .. '.mesh', nil, false) then
+    need_string(config .. '.mesh.id')
+    need_one_of(config .. '.mesh.mcast_rate', rates, false)
+    need_boolean(config .. '.mesh.disabled', false)
+  end
+end
+
+need_boolean('mesh_on_wan', false)
+need_boolean('mesh_on_lan', false)
