f458d788c8344e7f4461dff9f2b1c235c1188dcf
Matthias Schiffer <mschiffer@universe-factory.net>
2017-02-22 19:07:42 +0100

    gluon-config-mode-core: make welcome and reboot message optional
    Make use of our new _translate() function.

--- package/gluon-config-mode-core/luasrc/lib/gluon/config-mode/reboot/0900-msg-reboot.lua
+++ package/gluon-config-mode-core/luasrc/lib/gluon/config-mode/reboot/0900-msg-reboot.lua
@@ -7,7 +7,8 @@ local uci = require("simple-uci").cursor()
 local hostname = pretty_hostname.get(uci)
 local contact = uci:get_first('gluon-node-info', 'owner', 'contact')
 
-local msg = translate('gluon-config-mode:reboot')
+local msg = _translate('gluon-config-mode:reboot')
+if not msg then return end
 
 renderer.render_string(msg, {
 	hostname = hostname,
