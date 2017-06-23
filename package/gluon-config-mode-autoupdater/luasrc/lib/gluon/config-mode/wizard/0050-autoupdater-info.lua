872ec327cad1642934be563d6e30c75f8f4b0be7
rubo77 <github@r.z11.de>
2017-06-23 13:44:03 +0200

    gluon-config-mode-autoupdater: Autoupdater disabled message

--- package/gluon-config-mode-autoupdater/luasrc/lib/gluon/config-mode/wizard/0050-autoupdater-info.lua
+++ package/gluon-config-mode-autoupdater/luasrc/lib/gluon/config-mode/wizard/0050-autoupdater-info.lua
@@ -1,8 +1,14 @@
 return function(form, uci)
-	if uci:get_bool("autoupdater", "settings", "enabled") then
+	local enabled = uci:get_bool("autoupdater", "settings", "enabled")
+	if enabled then
 		form:section(
 			Section, nil,
 			translate('This node will automatically update its firmware when a new version is available.')
+		)
+	else
+		form:section(
+			Section, nil,
+			translate('Automatic updates are disabled. They can be enabled in <em>Advanced settings</em>.')
 		)
 	end
 end
