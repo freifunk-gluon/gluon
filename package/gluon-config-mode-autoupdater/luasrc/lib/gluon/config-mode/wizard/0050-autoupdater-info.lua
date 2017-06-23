b44373efaffced8a771adb14bda1973d13d61ebc
Ruben Barkow <rubo77@users.noreply.github.com>
2017-06-23 13:00:52 +0200

    gluon-web-*: remove unused Section variables (#1150)

--- package/gluon-config-mode-autoupdater/luasrc/lib/gluon/config-mode/wizard/0050-autoupdater-info.lua
+++ package/gluon-config-mode-autoupdater/luasrc/lib/gluon/config-mode/wizard/0050-autoupdater-info.lua
@@ -1,6 +1,6 @@
 return function(form, uci)
 	if uci:get_bool("autoupdater", "settings", "enabled") then
-		local s = form:section(
+		form:section(
 			Section, nil,
 			translate('This node will automatically update its firmware when a new version is available.')
 		)
