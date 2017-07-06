226d77c2a5ab42fc8047c2a08b519426581bbd73
rubo77 <github@r.z11.de>
2017-07-07 01:43:34 +0200

    travis: add gluon build

--- tests/validate_site.sh
+++ tests/validate_site.sh
@@ -5,3 +5,10 @@
 GLUON_SITEDIR="docs/site-example" lua5.1 scripts/site_config.lua
 
 bash -n scripts/*.sh
+
+cp -a docs/site-example site
+
+TARGET=ar71xx-generic
+make update GLUON_TARGET=$TARGET
+make clean GLUON_TARGET=$TARGET
+make GLUON_TARGET=$TARGET
