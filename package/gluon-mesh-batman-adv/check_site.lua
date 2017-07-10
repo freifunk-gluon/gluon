826ac884936d695252ade6b8b4b27a8383580f5e
Steffen Förster <ambassador86@gmail.com>
2017-07-10 23:08:39 +0200

    gluon-mesh-batman-adv: enable routing algorithm configuration through site.conf
    [Matthias Schiffer: move to proto_gluon_bat0_setup() and default to BATMAN_IV]

--- package/gluon-mesh-batman-adv/check_site.lua
+++ package/gluon-mesh-batman-adv/check_site.lua
@@ -1,3 +1,4 @@
 if need_table('mesh', nil, false) and  need_table('mesh.batman_adv', nil, false) then
-  need_number('mesh.batman_adv.gw_sel_class', false)
+	need_number('mesh.batman_adv.gw_sel_class', false)
+	need_one_of('mesh.batman_adv.routing_algo', {'BATMAN_IV', 'BATMAN_V'}, false)
 end
