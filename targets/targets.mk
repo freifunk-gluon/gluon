110d74768979c22eac388ec0252523c029408a45
kb-light <freifunk@kb-light.de>
2017-10-30 07:30:17 +0100

    targets: do not tag ubnt-erx(-sfp) as broken
    there is no wifi, so there is no bad wifi.

--- targets/targets.mk
+++ targets/targets.mk
@@ -4,6 +4,7 @@ $(eval $(call GluonTarget,ar71xx,nand))
 $(eval $(call GluonTarget,brcm2708,bcm2708))
 $(eval $(call GluonTarget,brcm2708,bcm2709))
 $(eval $(call GluonTarget,mpc85xx,generic))
+$(eval $(call GluonTarget,ramips,mt7621))
 $(eval $(call GluonTarget,x86,generic))
 $(eval $(call GluonTarget,x86,geode))
 $(eval $(call GluonTarget,x86,64))
@@ -14,7 +15,6 @@ $(eval $(call GluonTarget,brcm2708,bcm2710)) # BROKEN: Untested
 $(eval $(call GluonTarget,ipq806x)) # BROKEN: Untested
 $(eval $(call GluonTarget,mvebu)) # BROKEN: No AP+IBSS or 11s support
 $(eval $(call GluonTarget,ramips,mt7620)) # BROKEN: No AP+IBSS support
-$(eval $(call GluonTarget,ramips,mt7621)) # BROKEN: No AP+IBSS support, 11s has high packet loss
 $(eval $(call GluonTarget,ramips,mt7628)) # BROKEN: No AP+IBSS support
 $(eval $(call GluonTarget,ramips,rt305x)) # BROKEN: No AP+IBSS support
 $(eval $(call GluonTarget,sunxi)) # BROKEN: Untested
