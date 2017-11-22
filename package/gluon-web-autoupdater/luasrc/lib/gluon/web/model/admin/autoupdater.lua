b04e84a80d731df451bf6a5d5080e5761f1bee6b
kb-light <freifunk@kb-light.de>
2017-11-22 21:32:59 +0100

    gluon-web-autoupdater: introduce minute value

--- package/gluon-web-autoupdater/luasrc/lib/gluon/web/model/admin/autoupdater.lua
+++ package/gluon-web-autoupdater/luasrc/lib/gluon/web/model/admin/autoupdater.lua
@@ -32,8 +32,34 @@ function o:write(data)
 	uci:set("autoupdater", autoupdater, "branch", data)
 end
 
+o = s:option(Value, "minute", translate("Minute"), translate(
+	"This value forces the autoupdater to check for updates at the "
+	.. "specified minute. Normally there is no need to set this value "
+	.. "because it is selected automatically. You may want to set this to "
+	.. "a specific value if you have multiple nodes which should not "
+	.. "update at the same time."
+))
+o.datatype = "irange(0, 59)"
+o.default = uci:get("gluon", "autoupdater", "minute")
+o.optional = true
+
+function o:write(data)
+	if data == uci:get("gluon", "autoupdater", "minute") then
+		return
+	end
+	uci:set("gluon", "autoupdater", "minute", data)
+
+	if data then
+		local f = io.open("/usr/lib/micron.d/autoupdater", "w")
+		f:write(string.format("%i 4 * * * /usr/sbin/autoupdater\n", data))
+		f:write(string.format("%i 0-3,5-23 * * * /usr/sbin/autoupdater --fallback\n", data))
+		f:close()
+	end
+end
+
 function f:write()
 	uci:commit("autoupdater")
+	uci:commit("gluon")
 end
 
 return f
