75e956493bcada57b22293a0e5296be219787305
Julian Labus <julian@labus-online.de>
2018-02-06 12:43:48 +0100

    sunxi: LeMaker Banana Pi: remove BROKEN

--- targets/targets.mk
+++ targets/targets.mk
@@ -7,6 +7,7 @@ $(eval $(call GluonTarget,brcm2708,bcm2708))
 $(eval $(call GluonTarget,brcm2708,bcm2709))
 $(eval $(call GluonTarget,mpc85xx,generic))
 $(eval $(call GluonTarget,ramips,mt7621))
+$(eval $(call GluonTarget,sunxi))
 $(eval $(call GluonTarget,x86,generic))
 $(eval $(call GluonTarget,x86,geode))
 $(eval $(call GluonTarget,x86,64))
@@ -22,5 +23,4 @@ $(eval $(call GluonTarget,ar71xx,mikrotik)) # BROKEN: no sysupgrade support
 $(eval $(call GluonTarget,brcm2708,bcm2710)) # BROKEN: Untested
 $(eval $(call GluonTarget,ipq806x)) # BROKEN: Untested
 $(eval $(call GluonTarget,mvebu)) # BROKEN: No AP+IBSS or 11s support
-$(eval $(call GluonTarget,sunxi)) # BROKEN: Untested
 endif
