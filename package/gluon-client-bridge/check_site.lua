freifunk-gluon/gluon

commit 7ccdacd294e5fcb6718f806543bb2620c02c9ddb

treewide: rework check_site_lib.lua
In addition to significant internal differences in check_site_lib.lua (in
particular unifying error handling to a single place for the upcoming
multi-domain support), this changes the way fields are addressed in site
check scripts: rather than providing a string like 'next_node.ip6', the
path is passed as an array {'next_node', 'ip6'}.

Other changes in site check scripts:
* need_array and need_table now pass the full path to the sub fields to the
subcheck instead of the key and value
* Any check referring to a field inside a table implies that all higher
levels must be tables if they exist: a check for {'next_node', 'ip6'} adds
an implicit (optional) check for {'next_node'}, which allows to remove many
explicit checks for such tables

package/gluon-client-bridge/check_site.lua
@@ -1,15 +1,15 @@
-need_string_match(in_domain('next_node.mac'), '^%x[02468aAcCeE]:%x%x:%x%x:%x%x:%x%x:%x%x$', false)
+need_string_match(in_domain({'next_node', 'mac'}), '^%x[02468aAcCeE]:%x%x:%x%x:%x%x:%x%x:%x%x$', false)
 
-if need_string_match(in_domain('next_node.ip4'), '^%d+.%d+.%d+.%d+$', false) then
-	need_string_match(in_domain('prefix4'), '^%d+.%d+.%d+.%d+/%d+$')
+if need_string_match(in_domain({'next_node', 'ip4'}), '^%d+.%d+.%d+.%d+$', false) then
+	need_string_match(in_domain({'prefix4'}), '^%d+.%d+.%d+.%d+/%d+$')
 end
 
-need_string_match(in_domain('next_node.ip6'), '^[%x:]+$', false)
+need_string_match(in_domain({'next_node', 'ip6'}), '^[%x:]+$', false)
 
 
 for _, config in ipairs({'wifi24', 'wifi5'}) do
-	if need_table(config .. '.ap', nil, false) then
-		need_string(in_domain(config .. '.ap.ssid'))
-		need_boolean(config .. '.ap.disabled', false)
+	if need_table({config}, nil, false) then
+		need_string(in_domain({config, 'ap', 'ssid'}))
+		need_boolean({config, 'ap', 'disabled'}, false)
 	end
 end

package/gluon-authorized-keys/check_site.lua
@@ -1,1 +1,1 @@
-need_string_array(in_site('authorized_keys'))
+need_string_array(in_site({'authorized_keys'}))

package/gluon-autoupdater/check_site.lua
@@ -1,14 +1,10 @@
-need_string(in_site('autoupdater.branch'))
+need_string(in_site({'autoupdater', 'branch'}))
 
-local function check_branch(k, _)
-   assert_uci_name(k)
+need_table({'autoupdater', 'branches'}, function(branch)
+	need_alphanumeric_key(branch)
 
-   local prefix = string.format('autoupdater.branches[%q].', k)
-
-   need_string(in_site(prefix .. 'name'))
-   need_string_array_match(prefix .. 'mirrors', '^http://')
-   need_number(in_site(prefix .. 'good_signatures'))
-   need_string_array_match(in_site(prefix .. 'pubkeys'), '^%x+$')
-end
-
-need_table('autoupdater.branches', check_branch)
+	need_string(in_site(extend(branch, {'name'})))
+	need_string_array_match(extend(branch, {'mirrors'}), '^http://')
+	need_number(in_site(extend(branch, {'good_signatures'})))
+	need_string_array_match(in_site(extend(branch, {'pubkeys'})), '^%x+$')
+end)

package/gluon-config-mode-contact-info/check_site.lua
@@ -1,3 +1,1 @@
-if need_table(in_site('config_mode'), nil, false) and need_table(in_site('config_mode.owner'), nil, false) then
-  need_boolean(in_site('config_mode.owner.obligatory'), false)
-end
+need_boolean(in_site({'config_mode', 'owner', 'obligatory'}), false)

package/gluon-config-mode-geo-location/check_site.lua
@@ -1,3 +1,1 @@
-if need_table(in_site('config_mode'), nil, false) and need_table(in_site('config_mode.geo_location'), nil, false) then
-  need_boolean(in_site('config_mode.geo_location.show_altitude'), false)
-end
+need_boolean(in_site({'config_mode', 'geo_location', 'show_altitude'}), false)

package/gluon-core/check_site.lua
@@ -1,75 +1,57 @@
-need_string(in_site('site_code'))
-need_string(in_site('site_name'))
-need_string_match(in_domain('domain_seed'), '^' .. ('%x'):rep(64) .. '$')
+need_string(in_site({'site_code'}))
+need_string(in_site({'site_name'}))
+need_string_match(in_domain({'domain_seed'}), '^' .. ('%x'):rep(64) .. '$')
 
-if need_table('opkg', nil, false) then
-	need_string('opkg.lede', false)
+need_string({'opkg', 'lede'}, false)
+need_table({'opkg', 'extra'}, function(extra_repo)
+	need_alphanumeric_key(extra_repo)
+	need_string(extra_repo)
+end, false)
 
-	function check_repo(k, _)
-		-- this is not actually a uci name, but using the same naming rules here is fine
-		assert_uci_name(k)
+need_string(in_site({'hostname_prefix'}), false)
+need_string(in_site({'timezone'}))
 
-		local path = string.format('opkg.extra[%q]', k)
-		need_string(path)
-	end
+need_string_array({'ntp_servers'}, false)
 
-	need_table('opkg.extra', check_repo, false)
-end
-
-need_string(in_site('hostname_prefix'), false)
-need_string(in_site('timezone'))
-
-need_string_array('ntp_servers', false)
-
-need_string_match(in_domain('prefix6'), '^[%x:]+/64$')
+need_string_match(in_domain({'prefix6'}), '^[%x:]+/64$')
 
 
 for _, config in ipairs({'wifi24', 'wifi5'}) do
-	if need_table(config, nil, false) then
-		need_string(in_site('regdom')) -- regdom is only required when wifi24 or wifi5 is configured
+	if need_table({config}, nil, false) then
+		need_string(in_site({'regdom'})) -- regdom is only required when wifi24 or wifi5 is configured
 
-		need_number(config .. '.channel')
+		need_number({config, 'channel'})
 
 		local rates = {1000, 2000, 5500, 6000, 9000, 11000, 12000, 18000, 24000, 36000, 48000, 54000}
-		local supported_rates = need_array_of(in_site(config .. '.supported_rates'), rates, false)
-		if supported_rates then
-			need_array_of(config .. '.basic_rate', supported_rates, true)
-		else
-			need_array_of(config .. '.basic_rate', rates, false)
+		local supported_rates = need_array_of(in_site({config, 'supported_rates'}), rates, false)
+		need_array_of({config, 'basic_rate'}, supported_rates or rates, supported_rates ~= nil)
+
+		if need_table({config, 'ibss'}, nil, false) then
+			need_string(in_domain({config, 'ibss', 'ssid'}))
+			need_string_match(in_domain({config, 'ibss', 'bssid'}), '^%x[02468aAcCeE]:%x%x:%x%x:%x%x:%x%x:%x%x$')
+			need_one_of({config, 'ibss', 'mcast_rate'}, supported_rates or rates, false)
+			need_number({config, 'ibss', 'vlan'}, false)
+			need_boolean({config, 'ibss', 'disabled'}, false)
+		end
+
+		if need_table({config, 'mesh'}, nil, false) then
+			need_string(in_domain({config, 'mesh', 'id'}))
+			need_one_of({config, 'mesh', 'mcast_rate'}, supported_rates or rates, false)
+			need_boolean({config, 'mesh', 'disabled'}, false)
 		end
 	end
 end
 
-need_boolean(in_site('poe_passthrough'), false)
-if need_table('dns', nil, false) then
-	need_number('dns.cacheentries', false)
-	need_string_array_match('dns.servers', '^[%x:]+$', true)
+need_boolean(in_site({'poe_passthrough'}), false)
+
+if need_table({'dns'}, nil, false) then
+	need_string_array_match({'dns', 'servers'}, '^[%x:]+$')
+	need_number({'dns', 'cacheentries'}, false)
 end
 
-if need_table('next_node', nil, false) then
-	need_string_match(in_domain('next_node.ip6'), '^[%x:]+$', false)
-	need_string_match(in_domain('next_node.ip4'), '^%d+.%d+.%d+.%d+$', false)
-end
+need_string_match(in_domain({'next_node', 'ip6'}), '^[%x:]+$', false)
+need_string_match(in_domain({'next_node', 'ip4'}), '^%d+.%d+.%d+.%d+$', false)
 
-for _, config in ipairs({'wifi24', 'wifi5'}) do
-  local rates = {1000, 2000, 5500, 6000, 9000, 11000, 12000, 18000, 24000, 36000, 48000, 54000}
-  rates = need_array_of(in_site(config .. '.supported_rates'), rates, false) or rates
-
-  if need_table(config .. '.ibss', nil, false) then
-    need_string(in_domain(config .. '.ibss.ssid'))
-    need_string_match(in_domain(config .. '.ibss.bssid'), '^%x[02468aAcCeE]:%x%x:%x%x:%x%x:%x%x:%x%x$')
-    need_one_of(config .. '.ibss.mcast_rate', rates, false)
-    need_number(config .. '.ibss.vlan', false)
-    need_boolean(config .. '.ibss.disabled', false)
-  end
-
-  if need_table(config .. '.mesh', nil, false) then
-    need_string(in_domain(config .. '.mesh.id'))
-    need_one_of(config .. '.mesh.mcast_rate', rates, false)
-    need_boolean(config .. '.mesh.disabled', false)
-  end
-end
-
-need_boolean(in_site('mesh_on_wan'), false)
-need_boolean(in_site('mesh_on_lan'), false)
-need_boolean(in_site('single_as_lan'), false)
+need_boolean(in_site({'mesh_on_wan'}), false)
+need_boolean(in_site({'mesh_on_lan'}), false)
+need_boolean(in_site({'single_as_lan'}), false)

package/gluon-ebtables-source-filter/check_site.lua
@@ -1,2 +1,2 @@
-need_string_match(in_domain('prefix4'), '^%d+.%d+.%d+.%d+/%d+$', false)
-need_string_array_match(in_domain('extra_prefixes6'), '^[%x:]+/%d+$', false)
+need_string_match(in_domain({'prefix4'}), '^%d+.%d+.%d+.%d+/%d+$', false)
+need_string_array_match(in_domain({'extra_prefixes6'}), '^[%x:]+/%d+$', false)

package/gluon-mesh-batman-adv/check_site.lua
@@ -1,4 +1,2 @@
-if need_table('mesh', nil, false) and  need_table('mesh.batman_adv', nil, false) then
-	need_number('mesh.batman_adv.gw_sel_class', false)
-	need_one_of('mesh.batman_adv.routing_algo', {'BATMAN_IV', 'BATMAN_V'}, false)
-end
+need_number({'mesh', 'batman_adv', 'gw_sel_class'}, false)
+need_one_of({'mesh', 'batman_adv', 'routing_algo'}, {'BATMAN_IV', 'BATMAN_V'}, false)

commit 208cd4d98c7c4ffb782954f07c22207166c8f695

gluon-client-bridge: allow WLAN configs without ap section again
Fixes: 7ccdacd294e5 ("treewide: rework check_site_lib.lua")

package/gluon-client-bridge/check_site.lua
@@ -8,7 +8,7 @@ need_string_match(in_domain({'next_node', 'ip6'}), '^[%x:]+$', false)
 
 
 for _, config in ipairs({'wifi24', 'wifi5'}) do
-	if need_table({config}, nil, false) then
+	if need_table({config, 'ap'}, nil, false) then
 		need_string(in_domain({config, 'ap', 'ssid'}))
 		need_boolean({config, 'ap', 'disabled'}, false)
 	end