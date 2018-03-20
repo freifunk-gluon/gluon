717d6ea4c40be61d12792748156628c8e622b51e
Martin Weinelt <hexa@darmstadt.ccc.de>
2018-03-20 04:03:37 +0100

    fixup! gluon-config-mode-domain-select: allow hiding of individual domain codes

--- package/gluon-config-mode-domain-select/check_site.lua
+++ package/gluon-config-mode-domain-select/check_site.lua
@@ -1,2 +1,3 @@
 need_boolean(in_domain({'hide_domain'}), false)
-need_array_of(in_domain({'hide_domain_codes'}), table_keys(need_table(in_domain({'domain_names'}), nil, true)), false)
+valid_domain_codes = table_keys(need_table(in_domain({'domain_names'})))
+need_array_of(in_domain({'hide_domain_codes'}), valid_domain_codes, false)
