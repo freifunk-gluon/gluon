da7fbff1cfcac27032059d2ce989ea25c857aa47
Ralf Jung <post@ralfj.de>
2018-06-06 21:02:44 +0200

    add fallback text for gluon-config-mode:contact-note

--- package/gluon-config-mode-contact-info/luasrc/lib/gluon/config-mode/wizard/0500-contact-info.lua
+++ package/gluon-config-mode-contact-info/luasrc/lib/gluon/config-mode/wizard/0500-contact-info.lua
@@ -1,13 +1,15 @@
 return function(form, uci)
 	local pkg_i18n = i18n 'gluon-config-mode-contact-info'
+	local site_i18n = i18n 'gluon-site'
 
 	local site = require 'gluon.site'
 
 	local owner = uci:get_first("gluon-node-info", "owner")
 
-	local s = form:section(Section, nil, translate("gluon-config-mode:contact-help"))
+	local s = form:section(Section, nil, site_i18n.translate("gluon-config-mode:contact-help"))
 
-	local o = s:option(Value, "contact", pkg_i18n.translate("Contact info"), translate("gluon-config-mode:contact-note"))
+	local o = s:option(Value, "contact", pkg_i18n.translate("Contact info"),
+		site_i18n._translate("gluon-config-mode:contact-note") or pkg_i18n.translate("e.g. E-mail or phone number"))
 	o.default = uci:get("gluon-node-info", owner, "contact")
 	o.optional = true
 	function o:write(data)
