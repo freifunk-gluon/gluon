533ea986ad412c15e82a2c374fb63f050ecb926d
Andreas Ziegler <dev@andreas-ziegler.de>
2018-06-27 00:36:59 +0200

    gluon-config-mode-hostname: allow overriding hostname description text

--- package/gluon-config-mode-hostname/luasrc/lib/gluon/config-mode/wizard/0100-hostname.lua
+++ package/gluon-config-mode-hostname/luasrc/lib/gluon/config-mode/wizard/0100-hostname.lua
@@ -1,14 +1,16 @@
 return function(form, uci)
 	local pkg_i18n = i18n 'gluon-config-mode-hostname'
+	local site_i18n = i18n 'gluon-site'
 
 	local pretty_hostname = require 'pretty_hostname'
 	local site = require 'gluon.site'
 	local util = require 'gluon.util'
 
-	form:section(Section, nil, pkg_i18n.translate(
-		"The node name is used solely for identification of your node, e.g. on a "
-		.. "node map. It does not affect the name (SSID) of the broadcasted WLAN."
-	))
+	local help = site_i18n._translate("gluon-config-mode:hostname-help") or pkg_i18n.translate(
+		'The node name is used solely for identification of your node, e.g. on a '
+		.. 'node map. It does not affect the name (SSID) of the broadcasted WLAN.'
+	)
+	form:section(Section, nil, help)
 
 	local current_hostname = pretty_hostname.get(uci)
 	local default_hostname = util.default_hostname()
