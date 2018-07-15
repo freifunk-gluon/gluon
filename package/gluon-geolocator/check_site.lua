5ffb482705a28fd8dcfa23ae764a5adf8c03989e
Jan-Tarek Butt <tarek@ring0.de>
2018-07-16 01:33:28 +0200

    gluon-geolocator: update to master package configuration
    * drop bssid blacklist
    * only allow Master mode scanned wifis for osition request.

--- package/gluon-geolocator/check_site.lua
+++ package/gluon-geolocator/check_site.lua
@@ -1,3 +1,2 @@
 need_boolean(in_site({'geolocator', 'autolocation'}), false)
 need_number(in_site({'geolocator', 'interval'}), false)
-need_string_array_match(in_site({'geolocator', 'blacklist'}), '^%w+:%w+:%w+:%w+:%w+:%w+$')
