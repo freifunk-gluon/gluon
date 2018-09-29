c584d81952cb7aa11cfab5e329640244b8ddea9d
Matthias Schiffer <mschiffer@universe-factory.net>
2018-09-29 14:09:28 +0200

    gluon-config-mode-hostname: fix site check for prefill
    Do not require this setting in site.conf.

--- package/gluon-config-mode-hostname/check_site.lua
+++ package/gluon-config-mode-hostname/check_site.lua
@@ -1,2 +1,2 @@
 need_boolean(in_site({'config_mode', 'hostname', 'optional'}), false)
-need_boolean(in_site({'config_mode', 'hostname', 'prefill'}), true)
+need_boolean(in_site({'config_mode', 'hostname', 'prefill'}), false)
