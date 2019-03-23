b061a220eeaf033e7afee0d09fef009ceddec956
Jan-Tarek Butt <tarek@ring0.de>
2019-03-23 17:59:55 +0100

    gluon-config-mode-mesh-vpn: 0300-mesh-vpn.lua fix limit_ingress default value in case of default disabled simple-tc for tunneldigger
    Signed-off-by: Jan-Tarek Butt <tarek@ring0.de>

--- package/gluon-config-mode-mesh-vpn/luasrc/lib/gluon/config-mode/wizard/0300-mesh-vpn.lua
+++ package/gluon-config-mode-mesh-vpn/luasrc/lib/gluon/config-mode/wizard/0300-mesh-vpn.lua
@@ -48,10 +48,13 @@ return function(form, uci)
 
 	o = s:option(Value, "limit_ingress", pkg_i18n.translate("Downstream (kbit/s)"))
 	o:depends(limit, true)
+	o.default = uci:get("simple-tc", "mesh_vpn", "limit_ingress")
 	if has_tunneldigger then
-		o.default = uci:get("tunneldigger", "mesh_vpn", "limit_bw_down")
-	else
-		o.default = uci:get("simple-tc", "mesh_vpn", "limit_ingress")
+		-- Check if limit_bw_down exsist if not take value vom limit_ingress
+		local limit_bw_down = uci:get("tunneldigger", "mesh_vpn", "limit_bw_down")
+		if limit_bw_down ~= nil then
+			o.default = limit_bw_down
+		end
 	end
 	o.datatype = "uinteger"
 	function o:write(data)
