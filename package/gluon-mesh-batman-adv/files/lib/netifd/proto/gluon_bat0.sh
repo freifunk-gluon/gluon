b262b7256384e64166eeda1bfcfd0e504a018107
Matthias Schiffer <mschiffer@universe-factory.net>
2019-04-21 22:38:07 +0200

    gluon-mesh-batman-adv: adjust to removal of batman-adv UCI config

--- package/gluon-mesh-batman-adv/files/lib/netifd/proto/gluon_bat0.sh
+++ package/gluon-mesh-batman-adv/files/lib/netifd/proto/gluon_bat0.sh
@@ -8,6 +8,13 @@ proto_gluon_bat0_init_config() {
 	no_device=1
 	available=1
 	renew_handler=1
+
+	proto_config_add_string 'gw_mode'
+}
+
+lookup_site() {
+	local path="$1" default="$2"
+	lua -e "print(require('gluon.site').$path('$default'))"
 }
 
 proto_gluon_bat0_renew() {
@@ -36,16 +43,43 @@ proto_gluon_bat0_renew() {
 proto_gluon_bat0_setup() {
 	local config="$1"
 
+	local routing_algo=$(lookup_site 'mesh.batman_adv.routing_algo' 'BATMAN_IV')
+
+	local gw_mode
+	json_get_vars gw_mode
+
+	batctl routing_algo "$routing_algo"
+	batctl interface create
+
+	batctl orig_interval 5000
+	batctl hop_penalty 15
+	batctl multicast_mode 0
+
+	case "$gw_mode" in
+		server)
+			batctl gw_mode "server"
+		;;
+		client)
+			local gw_sel_class="$(lookup_site 'mesh.batman_adv.gw_sel_class')"
+			if [ -n "$gw_sel_class" ]; then
+				batctl gw_mode "client" "$gw_sel_class"
+			else
+				batctl gw_mode "client"
+			fi
+		;;
+		*)
+			batctl gw_mode "off"
+		;;
+	esac
+
+
 	local primary0_mac="$(lua -e 'print(require("gluon.util").generate_mac(3))')"
 
 	ip link add primary0 type dummy
 	echo 1 > /proc/sys/net/ipv6/conf/primary0/disable_ipv6
 	ip link set primary0 address "$primary0_mac" mtu 1532 up
 
-	local routing_algo="$(lua -e 'print(require("gluon.site").mesh.batman_adv.routing_algo())')"
-	(echo "$routing_algo" >/sys/module/batman_adv/parameters/routing_algo) 2>/dev/null
-
-	echo bat0 > /sys/class/net/primary0/batman_adv/mesh_iface
+	batctl interface add primary0
 
 	proto_init_update primary0 1
 	proto_send_update "$config"
@@ -56,7 +90,7 @@ proto_gluon_bat0_setup() {
 proto_gluon_bat0_teardown() {
 	local config="$1"
 
-	ip link del bat0
+	batctl interface destroy
 	ip link del primary0
 }
 
