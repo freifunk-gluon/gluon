3ec108aacc1fe88918cdd612e9ced9a5e6fef551
Matthias Schiffer <mschiffer@universe-factory.net>
2019-06-16 14:55:23 +0200

    gluon-web: fix Http:getcookie()
    None of our code uses cookies, so this issue went unnoticed.

--- package/gluon-web/luasrc/usr/lib/lua/gluon/web/http.lua
+++ package/gluon-web/luasrc/usr/lib/lua/gluon/web/http.lua
@@ -56,7 +56,7 @@ function Http:getcookie(name)
 	local c = string.gsub(";" .. (self:getenv("HTTP_COOKIE") or "") .. ";", "%s*;%s*", ";")
 	local p = ";" .. name .. "=(.-);"
 	local i, j, value = c:find(p)
-	return value and urldecode(value)
+	return value and protocol.urldecode(value)
 end
 
 function Http:getenv(name)
