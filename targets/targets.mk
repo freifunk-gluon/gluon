e23e2be8502400641966766db48a0079f51da327
heini66 <heini66@users.noreply.github.com>
2019-08-31 14:25:21 +0200

    Update targets.mk
    Patch 1 of 2
    Addd initial lantiq support

--- targets/targets.mk
+++ targets/targets.mk
@@ -26,5 +26,7 @@ ifneq ($(BROKEN),)
 $(eval $(call GluonTarget,ar71xx,mikrotik)) # BROKEN: no sysupgrade support
 $(eval $(call GluonTarget,brcm2708,bcm2710)) # BROKEN: Untested
 $(eval $(call GluonTarget,ipq806x)) # BROKEN: See target file for details
+$(eval $(call GluonTarget,lantiq,xrx200)) # BROKEN: No dedicated wan IF
+$(eval $(call GluonTarget,lantiq,xway)) # BROKEN: No dedicated wan IF
 $(eval $(call GluonTarget,mvebu,cortexa9)) # BROKEN: No AP+IBSS or 11s support
 endif
