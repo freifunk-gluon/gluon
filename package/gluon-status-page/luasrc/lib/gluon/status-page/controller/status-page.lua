21397ce678c32539247196135e3c30d914d5c592
lemoer <git@irrelefant.net>
2019-09-30 02:00:17 +0200

    gluon-status-page: add domain and site (#1820)

--- package/gluon-status-page/luasrc/lib/gluon/status-page/controller/status-page.lua
+++ package/gluon-status-page/luasrc/lib/gluon/status-page/controller/status-page.lua
@@ -82,5 +82,5 @@ entry({}, call(function(http, renderer)
 	end
 
 
-	renderer.render('status-page', { nodeinfo = nodeinfo }, 'gluon-status-page')
+	renderer.render('status-page', { nodeinfo = nodeinfo, site = site }, 'gluon-status-page')
 end))
