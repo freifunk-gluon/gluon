freifunk-gluon/gluon

commit 4249d65af73ee9228d827af913b7caf2aac00809

treewide: fix luacheck warnings

package/gluon-web-node-role/luasrc/lib/gluon/config-mode/model/admin/noderole.lua
@@ -17,8 +17,8 @@ s = f:section(Section, nil, translate(
 
 o = s:option(ListValue, "role", translate("Role"))
 o.default = role
-for _, role in ipairs(site.roles.list()) do
-	o:value(role, site_i18n.translate('gluon-web-node-role:role:' .. role))
+for _, role_value in ipairs(site.roles.list()) do
+	o:value(role, site_i18n.translate('gluon-web-node-role:role:' .. role_value))
 end
 
 function o:write(data)

package/gluon-authorized-keys/luasrc/lib/gluon/upgrade/100-authorized-keys
@@ -5,7 +5,7 @@ local file = '/etc/dropbear/authorized_keys'
 
 local keys = {}
 
-function load_keys()
+local function load_keys()
   for line in io.lines(file) do
     keys[line] = true
   end

package/gluon-config-mode-contact-info/luasrc/lib/gluon/config-mode/wizard/0500-contact-info.lua
@@ -2,8 +2,6 @@ return function(form, uci)
 	local pkg_i18n = i18n 'gluon-config-mode-contact-info'
 	local site_i18n = i18n 'gluon-site'
 
-	local site = require 'gluon.site'
-
 	local owner = uci:get_first("gluon-node-info", "owner")
 
 	local help = site_i18n._translate("gluon-config-mode:contact-help") or pkg_i18n.translate(

package/gluon-config-mode-domain-select/luasrc/lib/gluon/config-mode/wizard/0200-domain-select.lua
@@ -7,7 +7,8 @@ return function(form, uci)
 	local util = require 'gluon.util'
 
 	local selected_domain = uci:get('gluon', 'core', 'domain')
-	local configured = uci:get_first('gluon-setup-mode','setup_mode', 'configured') == '1' or (selected_domain ~= site.default_domain())
+	local configured = uci:get_first('gluon-setup-mode','setup_mode', 'configured') == '1' or
+		(selected_domain ~= site.default_domain())
 
 	local function hide_domain_code(domain, code)
 		if configured and code == selected_domain then

package/gluon-config-mode-geo-location-osm/luasrc/usr/lib/lua/gluon/config-mode/geo-location-osm.lua
@@ -9,7 +9,8 @@ M.MapValue = osm.MapValue
 function M.help(i18n)
 	local pkg_i18n = i18n 'gluon-config-mode-geo-location-osm'
 	return pkg_i18n.translate(
-		'You may also select the position on the map displayed below if your computer is connected to the internet at the moment.'
+		'You may also select the position on the map displayed below '
+		.. 'if your computer is connected to the internet at the moment.'
 	)
 end
 

package/gluon-config-mode-geo-location/luasrc/lib/gluon/config-mode/wizard/0400-geo-location.lua
@@ -43,7 +43,7 @@ return function(form, uci)
 		end
 	end
 
-	local map = {}
+	local map
 	if osm then
 		map = s:option(osm.MapValue, "map", osm.options())
 		map:depends(share_location, true)
@@ -56,7 +56,9 @@ return function(form, uci)
 	function o:write(data)
 		uci:set("gluon-node-info", location, "latitude", data)
 	end
-	map.lat = o
+	if osm then
+		map.lat = o
+	end
 
 	o = s:option(Value, "longitude", pkg_i18n.translate("Longitude"), pkg_i18n.translatef("e.g. %s", "10.689901"))
 	o.default = uci:get("gluon-node-info", location, "longitude")
@@ -65,7 +67,9 @@ return function(form, uci)
 	function o:write(data)
 		uci:set("gluon-node-info", location, "longitude", data)
 	end
-	map.lon = o
+	if osm then
+		map.lon = o
+	end
 
 	if show_altitude then
 		o = s:option(Value, "altitude",

package/gluon-config-mode-hostname/luasrc/lib/gluon/config-mode/wizard/0100-hostname.lua
@@ -14,7 +14,8 @@ return function(form, uci)
 
 	local current_hostname = pretty_hostname.get(uci)
 	local default_hostname = util.default_hostname()
-	local configured = uci:get_first('gluon-setup-mode', 'setup_mode', 'configured', false) or (current_hostname ~= default_hostname)
+	local configured = uci:get_first('gluon-setup-mode', 'setup_mode', 'configured', false) or
+		(current_hostname ~= default_hostname)
 
 	local s = form:section(Section)
 	local o = s:option(Value, "hostname", pkg_i18n.translate("Node name"))

package/gluon-core/check_site.lua
@@ -39,8 +39,8 @@ for _, config in ipairs({'wifi24', 'wifi5'}) do
 		elseif config == 'wifi5' then
 			local channels = {
 				34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58, 60, 62,
-				64, 96, 100, 102, 104, 106, 108, 110, 112, 114, 116, 118, 
-				120, 122, 124, 126, 128, 132, 134, 136, 138, 140, 142, 144, 
+				64, 96, 100, 102, 104, 106, 108, 110, 112, 114, 116, 118,
+				120, 122, 124, 126, 128, 132, 134, 136, 138, 140, 142, 144,
 				149, 151, 153, 155, 157, 159, 161, 165, 169, 173 }
 			need_one_of({config, 'channel'}, channels)
 			need_chanlist({config, 'outdoor_chanlist'}, channels, false)

commit a3a8d962fc3c9a07d84a7a4c9063c38bea0235fd

gluon-web-node-role: fix node role list
Fixes: 4249d65af73e ("treewide: fix luacheck warnings")
Closes: #1851

package/gluon-web-node-role/luasrc/lib/gluon/config-mode/model/admin/noderole.lua
@@ -18,7 +18,7 @@ s = f:section(Section, nil, translate(
 o = s:option(ListValue, "role", translate("Role"))
 o.default = role
 for _, role_value in ipairs(site.roles.list()) do
-	o:value(role, site_i18n.translate('gluon-web-node-role:role:' .. role_value))
+	o:value(role_value, site_i18n.translate('gluon-web-node-role:role:' .. role_value))
 end
 
 function o:write(data)