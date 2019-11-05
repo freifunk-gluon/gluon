335fae102523be155b87fe5abbc05bef96ede56f
Matthias Schiffer <mschiffer@universe-factory.net>
2019-11-05 20:49:55 +0100

    build: drop GLUON_WLAN_MESH variable

--- targets/targets.mk
+++ targets/targets.mk
@@ -1,5 +1,3 @@
-GLUON_WLAN_MESH_11s := $(filter 11s,$(GLUON_WLAN_MESH))
-
 $(eval $(call GluonTarget,ar71xx,generic))
 ifneq ($(GLUON_DEPRECATED),0)
 $(eval $(call GluonTarget,ar71xx,tiny))
@@ -7,25 +5,23 @@ endif
 $(eval $(call GluonTarget,ar71xx,nand))
 $(eval $(call GluonTarget,brcm2708,bcm2708))
 $(eval $(call GluonTarget,brcm2708,bcm2709))
+$(eval $(call GluonTarget,ipq40xx,generic))
+$(eval $(call GluonTarget,ipq806x,generic))
 $(eval $(call GluonTarget,lantiq,xway))
 $(eval $(call GluonTarget,mpc85xx,generic))
 $(eval $(call GluonTarget,mpc85xx,p1020))
+$(eval $(call GluonTarget,ramips,mt7620))
 $(eval $(call GluonTarget,ramips,mt7621))
+$(eval $(call GluonTarget,ramips,mt76x8))
+$(eval $(call GluonTarget,ramips,rt305x))
 $(eval $(call GluonTarget,sunxi,cortexa7))
 $(eval $(call GluonTarget,x86,generic))
 $(eval $(call GluonTarget,x86,geode))
 $(eval $(call GluonTarget,x86,64))
 
-ifneq ($(GLUON_WLAN_MESH_11s)$(BROKEN),)
-$(eval $(call GluonTarget,ipq40xx,generic))
-$(eval $(call GluonTarget,ipq806x,generic))
-$(eval $(call GluonTarget,ramips,mt7620))
-$(eval $(call GluonTarget,ramips,mt76x8))
-$(eval $(call GluonTarget,ramips,rt305x))
-endif
 
 ifneq ($(BROKEN),)
 $(eval $(call GluonTarget,ar71xx,mikrotik)) # BROKEN: no sysupgrade support
 $(eval $(call GluonTarget,brcm2708,bcm2710)) # BROKEN: Untested
-$(eval $(call GluonTarget,mvebu,cortexa9)) # BROKEN: No AP+IBSS or 11s support
+$(eval $(call GluonTarget,mvebu,cortexa9)) # BROKEN: No 11s support
 endif
