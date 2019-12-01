580b7357d5fe53e01c8845aec348fbae255b5e7f
Martin Weinelt <martin@darmstadt.freifunk.net>
2019-12-01 16:22:34 +0100

    Revert gluon-status-page gateway_nexthop changes
    This reverts commits
    - caf2dd037ba4b560a0794ce87d6a230e7a6cbcf8.
    - 07ebac6a49a288a3f5b7c7fc4e45a38b265ff929
    - 55eff45f96b435de2d3d94a0189ff99473e7828d
    
    I accidentally pushed these commits as I had them lying around on a
    dirty checkout I did testing on.

--- package/gluon-status-page/javascript/status-page.js
+++ package/gluon-status-page/javascript/status-page.js
@@ -112,20 +112,6 @@
 		'bytes': function(bytes) {
 			return prettyBytes(bytes);
 		},
-		'neighbour': function(addr) {
-			if (!addr)
-				return '';
-
-			for (var i in interfaces) {
-				var iface = interfaces[i];
-				var neigh = iface.get_neigh(addr);
-				if (!neigh)
-					continue;
-				return 'via ' + neigh.get_hostname() + ' (' + i + ')';
-			}
-
-			return 'via ' + addr + ' (unknown iface)';
-		}
 	}
 
 
@@ -596,9 +582,6 @@
 		}
 
 		return {
-			'get_hostname': function() {
-				return hostname.textContent;
-			},
 			'update_nodeinfo': function(nodeinfo) {
 				var addr = choose_address(nodeinfo.network.addresses);
 				if (addr) {
