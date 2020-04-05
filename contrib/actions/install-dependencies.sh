366ba8ab99ccc9f7761d1ff3af86c5f6bc99f785
David Bauer <mail@david-bauer.net>
2020-04-05 19:40:44 +0200

    contrib: actions: add custom sources.list

--- contrib/actions/install-dependencies.sh
+++ contrib/actions/install-dependencies.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+sudo cp contrib/actions/sources.list /etc/apt/sources.list
+sudo apt update
 sudo apt install git subversion build-essential python gawk unzip libncurses5-dev zlib1g-dev libssl-dev wget time || exit 1
 sudo apt clean
 sudo rm -rf /var/lib/apt/lists/*
