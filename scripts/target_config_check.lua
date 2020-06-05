freifunk-gluon/gluon

commit 9e23534ec365158768bb68699724b9ed559d1331

build: rework config generation
So far, we were using a sort operation on the generated .config to
implement precedence of =y packages over =m, and =m over unset.
Unfortunately, this sort not only used for packages, but for all config
lines. This made it impossible to override settings from targets/generic
in a target config when the new setting was sorted before the generic
setting.

To fix this, track configurations by their keys, so we can properly
override config keys that were set before. Value-based precedence is
only preserved for package configuration.

The config() and try_config() calls always take key and value as
separate arguments now. Strings are quoted automatically; the values
true, nil and false map to y, m and unset for tristate options. config()
can take an optional third argument to override the error message to
display when the setting fails to apply.

All existing target configs generate the same .config with the old and the
new code. The new code is also a bit faster on targets with many devices.

scripts/target_config_check.lua
@@ -1,16 +1,12 @@
-local errors = {}
+local errors = false
 
-
-local function fail(...)
-	if not next(errors) then
+local function fail(msg)
+	if not errors then
+		errors = true
 		io.stderr:write('Configuration failed:', '\n')
 	end
 
-	local msg = string.format(...)
-	if not errors[msg] then
-		errors[msg] = true
-		io.stderr:write(' * ', msg, '\n')
-	end
+	io.stderr:write(' * ', msg, '\n')
 end
 
 local function match_config(f)
@@ -23,49 +19,21 @@ local function match_config(f)
 	return false
 end
 
-local function check_config(pattern)
-	return match_config(function(line) return line == pattern end)
-end
-
-local function check_config_prefix(pattern)
-	return match_config(function(line) return string.sub(line, 1, -2) == pattern end)
+local function check_config(config)
+	return match_config(function(line) return line == config end)
 end
 
 
-local funcs = {}
+local lib = dofile('scripts/target_config_lib.lua')()
 
-function funcs.config_message(_, message, ...)
-	local pattern = string.format(...)
-
-	if not check_config(pattern) then
-		fail('%s', message)
-	end
-end
-
-function funcs.config_package(_, pkg, value)
-	local pattern = string.format('CONFIG_PACKAGE_%s=%s', pkg, value)
-	local res
-	if value == 'y' then
-		res = check_config(pattern)
-	else
-		res = check_config_prefix(string.sub(pattern, 1, -2))
-	end
-
-	if not res then
-		fail("unable to enable package '%s'", pkg)
-	end
-end
-
-local lib = dofile('scripts/target_config_lib.lua')(funcs)
-
-for config, v in pairs(lib.configs) do
-	if v == 2 then
-		if not check_config(config) then
-			fail("unable to set '%s'", config)
+for _, config in pairs(lib.configs) do
+	if config.required then
+		if not check_config(config:format()) then
+			fail(config.required)
 		end
 	end
 end
 
-if next(errors) then
+if errors then
 	os.exit(1)
 end

scripts/target_config.lua
@@ -1,24 +1,5 @@
-local funcs = {}
+local lib = dofile('scripts/target_config_lib.lua')()
 
-function funcs.config_message(config, _, ...)
-	config(...)
-end
-
-function funcs.config_package(config, pkg, value)
-	config('CONFIG_PACKAGE_%s=%s', pkg, value)
-end
-
-local lib = dofile('scripts/target_config_lib.lua')(funcs)
-
-
-local output = {}
-
-for config in pairs(lib.configs) do
-	table.insert(output, config)
-end
-
--- The sort will make =y entries override =m ones
-table.sort(output)
-for _, line in ipairs(output) do
-	io.stdout:write(line, '\n')
+for _, config in pairs(lib.configs) do
+	io.stdout:write(config:format(), '\n')
 end

scripts/target_config_lib.lua
@@ -40,7 +40,7 @@ local function compact_list(list, keep_neg)
 	return ret
 end
 
-return function(funcs)
+return function()
 	local lib = dofile('scripts/target_lib.lua')
 	local env = lib.env
 
@@ -102,12 +102,29 @@ END_MAKE
 		return pkgs
 	end
 
+	local enabled_packages = {}
+	-- Arguments: package name and config value (true: y, nil: m, false: unset)
+	-- Ensures precedence of y > m > unset
+	local function config_package(pkg, v)
+		if v == false then
+			if not enabled_packages[pkg] then
+				lib.try_config('CONFIG_PACKAGE_' .. pkg, false)
+			end
+			return
+		end
+
+		if v == true or not enabled_packages[pkg] then
+			lib.config('CONFIG_PACKAGE_' .. pkg, v, string.format("unable to enable package '%s'", pkg))
+			enabled_packages[pkg] = true
+		end
+	end
+
 	local function handle_target_pkgs(pkgs)
 		for _, pkg in ipairs(pkgs) do
 			if string.sub(pkg, 1, 1) == '-' then
-				lib.try_config('# CONFIG_PACKAGE_%s is not set', string.sub(pkg, 2))
+				config_package(string.sub(pkg, 2), false)
 			else
-				funcs.config_package(lib.config, pkg, 'y')
+				config_package(pkg, true)
 			end
 		end
 	end
@@ -118,8 +135,8 @@ END_MAKE
 	lib.check_devices()
 
 	if not lib.opkg then
-		lib.config '# CONFIG_SIGNED_PACKAGES is not set'
-		lib.config 'CONFIG_CLEAN_IPKG=y'
+		lib.config('CONFIG_SIGNED_PACKAGES', false)
+		lib.config('CONFIG_CLEAN_IPKG', true)
 		lib.packages {'-opkg'}
 	end
 
@@ -133,7 +150,7 @@ END_MAKE
 			local function handle_pkgs(pkgs)
 				for _, pkg in ipairs(pkgs) do
 					if string.sub(pkg, 1, 1) ~= '-' then
-						funcs.config_package(lib.config, pkg, 'm')
+						config_package(pkg, nil)
 					end
 					device_pkgs = append_to_list(device_pkgs, pkg)
 				end
@@ -144,11 +161,15 @@ END_MAKE
 			handle_pkgs(dev.options.packages or {})
 			handle_pkgs(site_packages(dev.image))
 
-			funcs.config_message(lib.config, string.format("unable to enable device '%s'", profile),
-				'CONFIG_TARGET_DEVICE_%s_DEVICE_%s=y', openwrt_config_target, profile)
-			lib.config('CONFIG_TARGET_DEVICE_PACKAGES_%s_DEVICE_%s="%s"',
-				openwrt_config_target, profile,
-				table.concat(device_pkgs, ' '))
+			lib.config(
+				string.format('CONFIG_TARGET_DEVICE_%s_DEVICE_%s', openwrt_config_target, profile),
+				true,
+				string.format("unable to enable device '%s'", profile)
+			)
+			lib.config(
+				string.format('CONFIG_TARGET_DEVICE_PACKAGES_%s_DEVICE_%s', openwrt_config_target, profile),
+				table.concat(device_pkgs, ' ')
+			)
 		end
 	else
 		-- x86 fallback: no devices

scripts/target_lib.lua
@@ -150,13 +150,50 @@ local function add_image(image)
 	table.insert(M.images[device], setmetatable(image, image_mt))
 end
 
-function F.try_config(...)
-	M.configs[string.format(...)] = 1
+
+local function format_config(k, v)
+	local format
+	if type(v) == 'string' then
+		format = '%s=%q'
+	elseif v == true then
+		format = '%s=y'
+	elseif v == nil then
+		format = '%s=m'
+	elseif v == false then
+		format = '# %s is not set'
+	else
+		format = '%s=%d'
+	end
+	return string.format(format, k, v)
 end
 
-function F.config(...)
-	M.configs[string.format(...)] = 2
+local config_mt = {
+	__index = {
+		format = function(config)
+			return format_config(config.key, config.value)
+		end,
+	}
+}
+
+local function do_config(k, v, required)
+	M.configs[k] = setmetatable({
+		key = k,
+		value = v,
+		required = required,
+	}, config_mt)
 end
+
+function F.try_config(k, v)
+	do_config(k, v)
+end
+
+function F.config(k, v, message)
+	if not message then
+		message = string.format("unable to set '%s'", format_config(k, v))
+	end
+	do_config(k, v, message)
+end
+
 
 function F.packages(pkgs)
 	for _, pkg in ipairs(pkgs) do

targets/ar71xx-generic
@@ -1,5 +1,5 @@
-config 'CONFIG_GLUON_SPECIALIZE_KERNEL=y'
-config 'CONFIG_TARGET_SQUASHFS_BLOCK_SIZE=64'
+config('CONFIG_GLUON_SPECIALIZE_KERNEL', true)
+config('CONFIG_TARGET_SQUASHFS_BLOCK_SIZE', 64)
 
 local ATH10K_PACKAGES = {
 	'kmod-ath10k',

targets/ar71xx-mikrotik
@@ -1,4 +1,4 @@
-config 'CONFIG_GLUON_SPECIALIZE_KERNEL=y'
+config('CONFIG_GLUON_SPECIALIZE_KERNEL', true)
 
 defaults {
 	factory = false,

targets/ar71xx-nand
@@ -1,4 +1,4 @@
-config 'CONFIG_GLUON_SPECIALIZE_KERNEL=y'
+config('CONFIG_GLUON_SPECIALIZE_KERNEL', true)
 
 local ATH10K_PACKAGES = {'kmod-ath10k', '-kmod-ath10k-ct', 'ath10k-firmware-qca988x', '-ath10k-firmware-qca988x-ct'}
 

targets/ar71xx-tiny
@@ -1,4 +1,4 @@
-config 'CONFIG_GLUON_SPECIALIZE_KERNEL=y'
+config('CONFIG_GLUON_SPECIALIZE_KERNEL', true)
 
 no_opkg()
 

commit 8b64517f1b3c4e46d2080c312243f96a6ac03366

build: target_config_check: make check more lenient
Always allow options set to builtin (=y) when modular setting (=m) is
expected. This can happen when a package is added explicitly (in a
target defintion or site.mk) that is also pulled in as a dependency of
another builtin package.

Fixes: 9e23534ec365 ("build: rework config generation")
Fixes: #2046

scripts/target_config_check.lua
@@ -9,18 +9,26 @@ local function fail(msg)
 	io.stderr:write(' * ', msg, '\n')
 end
 
-local function match_config(f)
-	for line in io.lines('openwrt/.config') do
-		if f(line) then
-			return true
-		end
+local function match_config(expected, actual)
+	if expected == actual then
+		return true
+	end
+
+	if expected:gsub('=m$', '=y') == actual then
+		return true
 	end
 
 	return false
 end
 
 local function check_config(config)
-	return match_config(function(line) return line == config end)
+	for line in io.lines('openwrt/.config') do
+		if match_config(config, line) then
+			return true
+		end
+	end
+
+	return false
 end
 
 