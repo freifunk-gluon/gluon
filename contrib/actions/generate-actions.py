8b451734c6280cb649ad8c1d2399a6dbb2782fd8
David Bauer <mail@david-bauer.net>
2020-09-17 22:33:36 +0200

    github: remove GitHub actions cache
    The performance benefit the cache brought was due to a broken sources
    CDN mirror handling in OpenWrt.
    
    The cache brings no measurable performance benefit. Disable it to slim
    down the pipeline steps.

--- contrib/actions/generate-actions.py
+++ contrib/actions/generate-actions.py
@@ -24,21 +24,10 @@ ACTIONS_TARGET="""
     runs-on: ubuntu-latest
     steps:
       - uses: actions/checkout@v1
-      - uses: actions/cache@v2
-        id: cache-dl
-        with:
-          path: dl_target
-          key: openwrt-dl-{target_name}-${{{{ hashFiles('modules') }}}}
-      - name: Prepare download cache
-        if: steps.cache-dl.outputs.cache-hit == 'true'
-        run: mkdir -p openwrt/dl; mv dl_target/* openwrt/dl/; ls openwrt/dl
       - name: Install Dependencies
         run: sudo contrib/actions/install-dependencies.sh
       - name: Build
         run: contrib/actions/run-build.sh {target_name}
-      - name: Create cache to save
-        if: steps.cache-dl.outputs.cache-hit != 'true'
-        run: mkdir dl_target; mv openwrt/dl/* dl_target/; find dl_target/ -size +20M -delete
       - name: Archive build logs
         if: ${{{{ !cancelled() }}}}
         uses: actions/upload-artifact@v1
