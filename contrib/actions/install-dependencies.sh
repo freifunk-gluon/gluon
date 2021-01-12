53b5139649becb73c3f06f0ef8f93bd9d2d7f5bd
David Bauer <mail@david-bauer.net>
2021-01-12 15:24:20 +0100

    docs contrib: add dependency on qemu-utils

--- contrib/actions/install-dependencies.sh
+++ contrib/actions/install-dependencies.sh
@@ -3,6 +3,6 @@
 set -e
 
 apt-get -y update
-apt-get -y install git subversion build-essential python gawk unzip libncurses5-dev zlib1g-dev libssl-dev wget time
+apt-get -y install git subversion build-essential python gawk unzip libncurses5-dev zlib1g-dev libssl-dev wget time qemu-utils
 apt-get -y clean
 rm -rf /var/lib/apt/lists/*
