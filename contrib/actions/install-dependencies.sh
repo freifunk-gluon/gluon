ca7a8ff528e5ef5a034070eed208a60aacf932ef
Vieno Hakkerinen <vieno@chemnitz.freifunk.net>
2021-04-05 21:33:04 +0200

    contrib/actions: use apt-get instead of apt
    apt does not have a stable CLI interface. Don't use it in scripts.

--- contrib/actions/install-dependencies.sh
+++ contrib/actions/install-dependencies.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-apt update
-apt install git subversion build-essential python gawk unzip libncurses5-dev zlib1g-dev libssl-dev wget time
-apt clean
+apt-get -y update
+apt-get -y install git subversion build-essential python gawk unzip libncurses5-dev zlib1g-dev libssl-dev wget time
+apt-get -y clean
 rm -rf /var/lib/apt/lists/*
