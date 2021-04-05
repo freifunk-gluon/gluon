1552a15dbe5091267137879a512366fa2a2039c0
Martin Weinelt <martin@darmstadt.freifunk.net>
2021-04-05 04:11:04 +0200

    contrib/actions: drop custom sources.list
    (cherry picked from commit 394bc8e6bdf4ae27a8fca3316db483fda5785075)

--- contrib/actions/install-dependencies.sh
+++ contrib/actions/install-dependencies.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-cp contrib/actions/sources.list /etc/apt/sources.list
-rm -rf /etc/apt/sources.list.d
 apt-get update
 apt-get install git subversion build-essential python gawk unzip libncurses-dev zlib1g-dev libssl-dev wget time
 apt-get clean
