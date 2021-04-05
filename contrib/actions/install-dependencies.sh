b31a57f060858ce3ddca4b1c367d8e5a3ae6bcb4
Martin Weinelt <martin@darmstadt.freifunk.net>
2021-04-06 01:00:01 +0200

    contrib/actions: drop version pin on libncurses-dev
    Fixes unmet dependencies:
    
     The following packages have unmet dependencies:
     libncurses5-dev : Depends: libtinfo5 (= 6.1-1ubuntu1.18.04) but 6.2-0ubuntu2 is to be installed
                       Depends: libncurses5 (= 6.1-1ubuntu1.18.04) but 6.2-0ubuntu2 is to be installed
                       Depends: libtinfo-dev (= 6.1-1ubuntu1.18.04)
     python : PreDepends: python-minimal (= 2.7.15~rc1-1) but it is not going to be installed
              Depends: libpython-stdlib (= 2.7.15~rc1-1) but it is not going to be installed
    E: Unable to correct problems, you have held broken packages.
    
    Switch to apt-get, because it has a stable API. Same as ca7a8ff52.

--- contrib/actions/install-dependencies.sh
+++ contrib/actions/install-dependencies.sh
@@ -4,7 +4,7 @@ set -e
 
 cp contrib/actions/sources.list /etc/apt/sources.list
 rm -rf /etc/apt/sources.list.d
-apt update
-apt install git subversion build-essential python gawk unzip libncurses5-dev zlib1g-dev libssl-dev wget time
-apt clean
+apt-get update
+apt-get install git subversion build-essential python gawk unzip libncurses-dev zlib1g-dev libssl-dev wget time
+apt-get clean
 rm -rf /var/lib/apt/lists/*
