eb3fad4bae30ff0c7912d94ffd3c7704cb0bdfe6
Matthias Schiffer <mschiffer@universe-factory.net>
2021-06-13 00:05:56 +0200

    build: move check_site.lua main script into gluon-core package
    Installing the script into the image allows site checks to run for
    manual package installations via opkg.

--- package/gluon.mk
+++ package/gluon.mk
@@ -21,7 +21,7 @@ shell-verbatim = $(call shell-unescape,$(call shell-escape,$(1)))
 
 
 define GluonCheckSite
-[ -z "$$IPKG_INSTROOT" ] || "${TOPDIR}/staging_dir/hostpkg/bin/lua" "${TOPDIR}/../scripts/check_site.lua" <<'END__GLUON__CHECK__SITE'
+[ -z "$$IPKG_INSTROOT" ] || "${TOPDIR}/staging_dir/hostpkg/bin/lua" "$$IPKG_INSTROOT/lib/gluon/check-site.lua" <<'END__GLUON__CHECK__SITE'
 $(call shell-verbatim,cat '$(1)')
 END__GLUON__CHECK__SITE
 endef
