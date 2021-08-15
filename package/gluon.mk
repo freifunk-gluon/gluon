2c352be439d77be37924e8d70e305784dc8208b7
lemoer <git@irrelefant.net>
2021-08-15 21:52:52 +0200

    gluon.mk: export LUA_CPATH again
    Without exporting, it did not work.

--- package/gluon.mk
+++ package/gluon.mk
@@ -24,12 +24,9 @@ shell-verbatim = $(call shell-unescape,$(call shell-escape,$(1)))
 # for the normal gluon build but is necessary for the imagebuilder.
 # Note that all variables in GluonCheckSite except for $(1) are expanded when
 # opkg calls the post-install scripts, not during the package build.
-#
-# [ -z "$$STAGING_DIR_HOSTPKG" ] || export LUA_CPATH="$${STAGING_DIR_HOSTPKG}/lib/lua/5.1/?.so"
-#
-# TODO: But maybe we do not need it anymore, since PATH is now altered?
 define GluonCheckSite
 [ -z "$$STAGING_DIR_HOSTPKG" ] || PATH="$$STAGING_DIR_HOSTPKG/bin:$$PATH"
+[ -z "$$STAGING_DIR_HOSTPKG" ] || export LUA_CPATH="$${STAGING_DIR_HOSTPKG}/lib/lua/5.1/?.so"
 lua "$$IPKG_INSTROOT/lib/gluon/check-site.lua" <<'END__GLUON__CHECK__SITE'
 $(call shell-verbatim,cat '$(1)')
 END__GLUON__CHECK__SITE
