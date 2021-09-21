b5db6435e5dec85498e3b78f76d33b6cfbc63815
Matthias Schiffer <mschiffer@universe-factory.net>
2021-09-21 17:55:44 +0200

    build: pass GLUON_VERSION and GLUON_SITE_VERSION to build
    A helper script is introduced to get the version from `git describe`.

---
+++ scripts/getversion.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if [ $# -ne 1 ]; then
+	echo >&2 "Usage: getversion.sh <directory>"
+	exit 1
+fi
+
+cd "$1" || exit 1
+
+git --git-dir=.git describe --always --abbrev=7 --dirty=+ 2>/dev/null && exit 0
+
+echo unknown
