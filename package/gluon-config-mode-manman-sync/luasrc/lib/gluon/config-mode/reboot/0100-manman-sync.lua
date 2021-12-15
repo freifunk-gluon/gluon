4dcf3e02124dbe0f644db30404f57259aa8dcc42
Maciej Krüger <mkg20001@gmail.com>
2021-12-15 07:34:36 +0100

    mamman-sync: more wip

--- package/gluon-config-mode-manman-sync/luasrc/lib/gluon/config-mode/reboot/0100-manman-sync.lua
+++ package/gluon-config-mode-manman-sync/luasrc/lib/gluon/config-mode/reboot/0100-manman-sync.lua
@@ -13,5 +13,5 @@ end
 if not msg then return end
 
 renderer.render_string(msg, {
-	node_id = uci:get("gluon", "manman_sync", "node_id")
+	location_id = uci:get("gluon", "manman_sync", "location_id")
 })
