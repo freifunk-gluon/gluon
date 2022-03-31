d6335865ea3857c21c9e204200d3577e233cebdf
Maciej Krüger <mkg20001@gmail.com>
2022-03-31 23:57:29 +0200

    mesh-olsr12-vxlan: init

---
+++ package/gluon-mesh-olsr12-vxlan/check_site.lua
@@ -0,0 +1,4 @@
+if need_boolean({'mesh', 'olsrd', 'olsr12', 'enable'}, false) then
+  need_number({'mesh', 'olsrd', 'olsr12', 'port'}, false)
+  need_string({'mesh', 'olsrd', 'olsr12', 'server'})
+end
