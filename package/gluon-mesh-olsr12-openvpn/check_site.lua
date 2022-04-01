34373e92a5c99ba8dfdf787df91569afa66dbd57
Maciej Krüger <mkg20001@gmail.com>
2022-04-01 13:52:17 +0200

    mesh-olsr12-openvpn: init

--- package/gluon-mesh-olsr12-openvpn/check_site.lua
+++ package/gluon-mesh-olsr12-openvpn/check_site.lua
@@ -1,4 +1,5 @@
 if need_boolean({'mesh', 'olsrd', 'olsr12', 'enable'}, false) then
   need_number({'mesh', 'olsrd', 'olsr12', 'port'}, false)
+  need_string({'mesh', 'olsrd', 'olsr12', 'ca'})
   need_string({'mesh', 'olsrd', 'olsr12', 'server'})
 end
