dcfa4522b3fc22f8a41f1e7372ca8000917f61d4
Matthias Schiffer <mschiffer@universe-factory.net>
2022-05-04 20:08:17 +0200

    Gluon v2021.1.2

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = '2015-2022, Project Gluon'
 author = 'Project Gluon'
 
 # The short X.Y version
-version = '2021.1.1'
+version = '2021.1.2'
 # The full version, including alpha/beta/rc tags
 release = version
 
