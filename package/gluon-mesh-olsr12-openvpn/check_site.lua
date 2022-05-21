47995565d358bcfc3535ec65e4c2534d75449bbe
Maciej Krüger <mkg20001@gmail.com>
2022-05-21 07:49:58 +0200

    cleanup cm

--- package/gluon-mesh-olsr12-openvpn/check_site.lua
+++
@@ -1,5 +0,0 @@
-if need_boolean({'mesh', 'olsrd', 'olsr12', 'enable'}, false) then
-  need_number({'mesh', 'olsrd', 'olsr12', 'port'}, false)
-  need_string({'mesh', 'olsrd', 'olsr12', 'ca'})
-  need_string({'mesh', 'olsrd', 'olsr12', 'server'})
-end
