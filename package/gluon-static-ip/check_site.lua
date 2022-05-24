744df67d49188d45430047c2e77389d7071bc0f2
Maciej Krüger <mkg20001@gmail.com>
2022-05-24 10:14:57 +0200

    cleanup cm

--- package/gluon-static-ip/check_site.lua
+++
@@ -1,8 +0,0 @@
-if need_string({'node_prefix4'}, false) then -- optional
-  need_number({'node_prefix4_range'}, true)
-  need_boolean({'node_prefix4_temporary'}, false)
-end
-
-need_string({'node_prefix6'}, true) -- always required
-need_number({'node_prefix6_range'}, false)
-need_boolean({'node_prefix6_temporary'}, false)
--- package/gluon-web-static-ip/luasrc/lib/gluon/config-mode/controller/admin/staticip.lua
+++
@@ -1,3 +0,0 @@
-package 'gluon-web-static-ip'
-
-entry({"admin", "staticip"}, model("admin/staticip"), _("Static IPs"), 30)
