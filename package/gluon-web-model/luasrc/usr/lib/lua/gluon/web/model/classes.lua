dcb8efd90f4220467b4cb77fed552fd657c414c6
Maciej Krüger <mkg20001@gmail.com>
2022-05-27 15:56:43 +0200

    fixup! model template method

--- package/gluon-web-model/luasrc/usr/lib/lua/gluon/web/model/classes.lua
+++ package/gluon-web-model/luasrc/usr/lib/lua/gluon/web/model/classes.lua
@@ -196,25 +196,6 @@ function Template:__init__(template)
 	self.template = template
 end
 
-local Warning = class(Node)
-M.Warning = Warning
-
-function Warning:__init__(...)
-	Node.__init__(self, ...)
-
-	self.default   = nil
-	self.size      = nil
-	self.optional  = false
-
-	self.template  = "model/warning"
-
-	self.error = false
-end
-
-function Warning:setcontent(content)
-	self.content = content
-end
-
 local AbstractValue = class(Node)
 M.AbstractValue = AbstractValue
 
@@ -445,6 +426,11 @@ function Section:option(t, ...)
 	return obj
 end
 
+function Section:template(...)
+	local obj  = Template(...)
+	self:append(obj)
+	return obj
+end
 
 local Form = class(Node)
 M.Form = Form
