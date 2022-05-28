31b6e10f69a9b437d9fc69d11e2e9589e09f184c
Maciej Krüger <mkg20001@gmail.com>
2022-05-28 11:44:53 +0200

    mesh-vpn-openvpn: move to cm

--- package/gluon-mesh-vpn-openvpn/check_site.lua
+++
@@ -1,4 +0,0 @@
-need_number({'mesh_vpn', 'openvpn', 'mtu'})
-need_boolean({'mesh_vpn', 'openvpn', 'self_signed'}, false)
-need_string({'mesh_vpn', 'openvpn', 'ca'})
-need_table({'mesh_vpn', 'openvpn', 'config'})
