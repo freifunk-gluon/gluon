acd6e0ccbcc716106cd65cce73e440aa0c4ec5f4
Maciej Krüger <mkg20001@gmail.com>
2022-06-12 20:04:24 +0200

    cleanup cm

--- package/gluon-web-private-ap/luasrc/lib/gluon/config-mode/controller/admin/privateap.lua
+++
@@ -1,8 +0,0 @@
-local uci = require("simple-uci").cursor()
-local wireless = require 'gluon.wireless'
-
-package 'gluon-web-private-ap'
-
-if wireless.device_uses_wlan(uci) then
-	entry({"admin", "privateap"}, model("admin/privateap"), _("Private AP"), 31)
-end
