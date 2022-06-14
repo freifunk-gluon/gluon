2d481765ee1732ec4f8066bad284800fd642b630
aiyion.prime <git@aiyionpri.me>
2022-06-14 09:33:01 +0200

    ath79-generic: (re)add support for NanoStation Loco M2 (XW)
    Gone due to
    commit 45c84a117bf8 ("ar71xx: drop target")

--- package/gluon-core/luasrc/usr/lib/lua/gluon/platform.lua
+++ package/gluon-core/luasrc/usr/lib/lua/gluon/platform.lua
@@ -38,6 +38,7 @@ function M.is_outdoor_device()
 		'tplink,wbs210-v1',
 		'tplink,wbs210-v2',
 		'ubnt,nanobeam-m5-xw',
+		'ubnt,nanostation-loco-m-xw',
 		'ubnt,nanostation-m-xw',
 		'ubnt,unifi-ap-outdoor-plus',
 		'ubnt,unifiac-mesh',
