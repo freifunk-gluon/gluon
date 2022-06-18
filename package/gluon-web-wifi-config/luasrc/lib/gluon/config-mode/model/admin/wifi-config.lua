410a52e3a453cb92902074ef5f08072485dfcd8c
Maciej Krüger <mkg20001@gmail.com>
2022-06-18 19:27:08 +0200

    60g fixes

--- package/gluon-web-wifi-config/luasrc/lib/gluon/config-mode/model/admin/wifi-config.lua
+++ package/gluon-web-wifi-config/luasrc/lib/gluon/config-mode/model/admin/wifi-config.lua
@@ -128,7 +128,7 @@ uci:foreach('wireless', 'wifi-device', function(config)
 		local mode = p:option(ListValue, radio .. '_p2pmode', translate("P2P Mode"), translate("Master=AP Slave=Station"))
 		mode.default = uci:get('wireless', name6, 'mode') or 'ap'
 		mode:value('ap', translate('Master'))
-		mode:value('station', translate('Slave'))
+		mode:value('sta', translate('Slave'))
 		mode:depends(vif, true)
 		function mode:write(data)
 			uci:set('wireless', name6, 'mode', data)
