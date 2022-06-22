4ab486dd2c8950869eca7f32910c4410edfb7fdf
aiyion.prime <git@aiyionpri.me>
2022-06-22 14:56:12 +0200

    run-build.sh, gluon_bat0.sh: add double quotes
    to prevent word splitting and globbing

--- contrib/actions/run-build.sh
+++ contrib/actions/run-build.sh
@@ -6,7 +6,7 @@ export BROKEN=1
 export GLUON_AUTOREMOVE=1
 export GLUON_DEPRECATED=1
 export GLUON_SITEDIR="contrib/ci/minimal-site"
-export GLUON_TARGET=$1
+export GLUON_TARGET="$1"
 export BUILD_LOG=1
 
 make update
