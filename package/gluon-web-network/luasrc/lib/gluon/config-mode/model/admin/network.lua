19f3c9d39686a972987ee89812fbdd5ab6322475
lemoer <git@irrelefant.net>
2023-02-26 22:46:23 +0100

    gluon-web-network: migrate to SPDX headers for licensing

--- package/gluon-web-network/luasrc/lib/gluon/config-mode/model/admin/network.lua
+++ package/gluon-web-network/luasrc/lib/gluon/config-mode/model/admin/network.lua
@@ -1,12 +1,6 @@
---[[
-Copyright 2014 Nils Schneider <nils@nilsschneider.net>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-http://www.apache.org/licenses/LICENSE-2.0
-]]--
+-- SPDX-License-Identifier: Apache-2.0
+-- SPDX-FileCopyrightText: 2014, Nils Schneider <nils@nilsschneider.net>
+-- SPDX-FileCopyrightText: 2023, Leonardo Mörlein <me@irrelefant.net>
 
 local uci = require("simple-uci").cursor()
 
