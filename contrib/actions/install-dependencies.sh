b70b9d2a0535017daee364aab95a48d028bd47f6
Florian Maurer <f.maurer@outlook.de>
2023-04-01 18:17:59 +0200

    update install dependency docs * add missing libelf-dev dependency (to build x86-64) * remove subversion dependency * add rsync dependency * add ecdsautils to docs

--- contrib/actions/install-dependencies.sh
+++ contrib/actions/install-dependencies.sh
@@ -3,6 +3,6 @@
 set -e
 
 apt-get -y update
-apt-get -y install git subversion build-essential python3 gawk unzip libncurses5-dev zlib1g-dev libssl-dev wget time qemu-utils
+apt-get -y install git build-essential python3 gawk unzip libncurses5-dev zlib1g-dev libssl-dev libelf-dev wget rsync time qemu-utils
 apt-get -y clean
 rm -rf /var/lib/apt/lists/*
