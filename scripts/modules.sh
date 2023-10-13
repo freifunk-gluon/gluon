4954ae645ebe2eb1577dd1011535e99320dfb378
Matthias Schiffer <mschiffer@universe-factory.net>
2023-10-13 20:22:27 +0200

    scripts/modules.sh: move site feeds to the front of the module list
    We don't want OpenWrt packages to replace Gluon packages by accident;
    the same logic applies to packages from site feeds.
    
    This gives site feeds a higher precedence than gluon/packages, but that
    is unlikely to cause any issues. Gluon base packages still have an even
    higher precedence.
    
    (cherry picked from commit 5d7aad74cf04e8d3a0314c91a4aa246f8a4794a9)

--- scripts/modules.sh
+++ scripts/modules.sh
@@ -2,7 +2,7 @@
 [ ! -f "$GLUON_SITEDIR"/modules ] || . "$GLUON_SITEDIR"/modules
 
 # shellcheck disable=SC2086
-FEEDS="$(echo $GLUON_FEEDS $GLUON_SITE_FEEDS | tr ' ' '\n')"
+FEEDS="$(echo $GLUON_SITE_FEEDS $GLUON_FEEDS | tr ' ' '\n')"
 
 GLUON_MODULES=openwrt
 
