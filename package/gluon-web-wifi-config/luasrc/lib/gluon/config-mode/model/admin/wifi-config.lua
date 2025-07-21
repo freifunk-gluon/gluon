4ff84aeaa47a0b06b0212804e66174b717ee53cc
Florian Maurer <f.maurer@outlook.de>
2025-07-21 21:00:32 +0200

    gluon-web: switch to notice instead of warning for deactivated mesh on third radio
    gluon-web-wifi-config: refine messages

--- package/gluon-web-wifi-config/luasrc/lib/gluon/config-mode/model/admin/wifi-config.lua
+++ package/gluon-web-wifi-config/luasrc/lib/gluon/config-mode/model/admin/wifi-config.lua
@@ -95,7 +95,7 @@ uci:foreach('wireless', 'wifi-device', function(config)
 
 	vif_option('client', {'client', 'owe'}, translate('Enable client network (access point)'))
 
-	-- disable setting mesh iface setting if radio does not support configured mesh channel
+	-- only show mesh iface setting on radios which support the configured mesh channel
 	if wireless.supports_channel(config, tonumber(config.channel)) then
 		local mesh_vif = vif_option('mesh', {'mesh'}, translate("Enable mesh network (802.11s)"))
 
@@ -104,8 +104,8 @@ uci:foreach('wireless', 'wifi-device', function(config)
 		end
 	else
 		p:element('model/warning', {
-			content = translate("Mesh deactivated due to unsupported channel"),
-		}, 'warning')
+			content = translate("The configured channel for meshing is unsupported by this WLAN radio."),
+		}, 'notice')
 	end
 
 	local phy = wireless.find_phy(config)
